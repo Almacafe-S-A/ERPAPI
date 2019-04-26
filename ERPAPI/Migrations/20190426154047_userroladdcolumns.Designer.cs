d937679b7bb60ed5bb5b4c17d2bdea63bb96ace7
Freddy Chinchilla <freddy,chinchilla@bi-dss.com>
2019-04-26 10:04:21 -0600

    add columns

--- ERPAPI/Migrations/20190426154047_userroladdcolumns.Designer.cs
+++ ERPAPI/Migrations/20190426154047_userroladdcolumns.Designer.cs
@@ -4,14 +4,16 @@ using ERP.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ERPAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190426154047_userroladdcolumns")]
+    partial class userroladdcolumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -27,21 +29,11 @@ namespace ERPAPI.Migrations
                     b.Property<string>("ConcurrencyStamp")
                         .IsConcurrencyToken();
 
-                    b.Property<DateTime>("FechaCreacion");
-
-                    b.Property<DateTime>("FechaModificacion");
-
                     b.Property<string>("Name")
                         .HasMaxLength(256);
 
                     b.Property<string>("NormalizedName")
                         .HasMaxLength(256);
-
-                    b.Property<string>("UsuarioCreacion")
-                        .IsRequired();
-
-                    b.Property<string>("UsuarioModificacion")
-                        .IsRequired();
 
                     b.HasKey("Id");
 
--- ERPAPI/Models/Role.cs
+++ ERPAPI/Models/Role.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,21 @@ namespace ERPAPI.Models
 {
     public class ApplicationRole : IdentityRole<Guid>
     {
-       // public string rolename { get; set; }
+        // public string rolename { get; set; }
+
+ 
+
+        [Required]
+        public string UsuarioCreacion { get; set; }
+
+        [Required]
+        public string UsuarioModificacion { get; set; }
+
+        [Required]
+        public DateTime FechaCreacion { get; set; }
+
+        [Required]
+        public DateTime FechaModificacion { get; set; }
+
     }
 }
