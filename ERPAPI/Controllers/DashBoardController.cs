6156778f6038b815c512004c4a4b0242f9768a0d
Freddy Chinchilla <freddy.chinchilla@bi-dss.com>
2019-05-21 18:34:37 -0600

    Dashboard mejoras

--- ERPAPI/Controllers/DashBoardController.cs
+++ ERPAPI/Controllers/DashBoardController.cs
@@ -7,6 +7,7 @@ using ERPAPI.Helpers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace ERPAPI.Controllers
@@ -62,5 +63,111 @@ namespace ERPAPI.Controllers
               
             
         }
+
+        [HttpPost("[action]")]
+        public ActionResult FacturacionByProductByYear([FromBody]Fechas _Fecha)
+        {
+
+            try
+            {
+                _Fecha.FechaInicio = Convert.ToDateTime(_Fecha.Year + "-01-01");
+                _Fecha.FechaFin = Convert.ToDateTime(_Fecha.Year + "-12-31");
+
+                var result = (from allSales in
+                             (from o in _context.Invoice
+                              .OrderBy(q => q.InvoiceDate)
+                              where o.InvoiceDate >= _Fecha.FechaInicio && o.InvoiceDate <= _Fecha.FechaFin
+                              && o.InvoiceDate != null && o.ProductId == _Fecha.Id //&& o.estado != "Anulada"
+                              select new
+                              {
+                                  Date = o.InvoiceDate,
+                                  Sales = o.Total,
+                              }
+                                 ).ToList()
+                              group allSales by new DateTime(allSales.Date.Year, allSales.Date.Month, 1) into g
+                              select new FacturacionMensual
+                              {
+                                  Facturacion = g.Sum(x => x.Sales),
+                                  Date = g.Key,
+                              }
+                           ).ToList();
+
+
+                for (int i = 1; i <= 12; i++)
+                {
+                    bool existe = result.Where(q => q.Date.Month == i).Count() > 0 ? true : false;
+                    if (!existe)
+                    {
+                        result.Add(new FacturacionMensual
+                        { Date = Convert.ToDateTime(_Fecha.Year + "-" + i.ToString().PadLeft(2,'0') + "-01"), Facturacion = 0 });
+                    }
+                }
+
+                result = result.OrderBy(q => q.Date).ToList();
+
+                return Json(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Ocurrio un error: { ex.ToString() }");
+                return BadRequest($"Ocurrio un error:{ex.Message}");
+            }
+
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult<Int32>> GetQuantityInvoices()
+        {
+            try
+            {
+                var Items = await _context.Invoice.CountAsync();
+                return await Task.Run(() => Ok(Items));
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Ocurrio un error: { ex.ToString() }");
+                return BadRequest($"Ocurrio un error:{ex.Message}");
+            }
+
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult<Int32>> GetQuantitySalesOrders()
+        {
+            try
+            {
+                var Items = await _context.SalesOrder.CountAsync();
+                return await Task.Run(() => Ok(Items));
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Ocurrio un error: { ex.ToString() }");
+                return BadRequest($"Ocurrio un error:{ex.Message}");
+            }
+
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult<Int32>> GetQuantityCertificadoDeposito()
+        {
+            try
+            {
+                var Items = await _context.CertificadoDeposito.CountAsync();
+                return await Task.Run(() => Ok(Items));
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Ocurrio un error: { ex.ToString() }");
+                return BadRequest($"Ocurrio un error:{ex.Message}");
+            }
+
+        }
+
+
+
+
     }
 }
--- ERPAPI/Helpers/Fechas.cs
+++ ERPAPI/Helpers/Fechas.cs
@@ -9,5 +9,18 @@ namespace ERPAPI.Helpers
     {
         public DateTime FechaInicio { get; set; }
         public DateTime FechaFin { get; set; }
+
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+
+        public Int64 Id { get; set; }
+    }
+
+    public partial class FacturacionMensual
+    {
+        public Nullable<int> FacturacionID { get; set; }
+        public Nullable<double> Facturacion { get; set; }
+        public System.DateTime Date { get; set; }
     }
 }
