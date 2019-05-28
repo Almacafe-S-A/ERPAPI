ca98267e6dc48de861a8e6a8b3560566e652b6a5
Freddy Chinchilla <freddy.chinchilla@bi-dss.com>
2019-05-28 11:01:16 -0600

    Se agrego la boleta de peso , que es la relacion con la balanza que cuenta ALMACAFE.

---
+++ ERPAPI/Migrations/20190528163352_addWeightBallot.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERPAPI.Migrations
+{
+    public partial class addWeightBallot : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "WeightBallot",
+                table: "GoodsReceived",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GoodsReceivedLine_GoodsReceivedId",
+                table: "GoodsReceivedLine",
+                column: "GoodsReceivedId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ControlPalletsLine_ControlPalletsId",
+                table: "ControlPalletsLine",
+                column: "ControlPalletsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ControlPalletsLine_ControlPallets_ControlPalletsId",
+                table: "ControlPalletsLine",
+                column: "ControlPalletsId",
+                principalTable: "ControlPallets",
+                principalColumn: "ControlPalletsId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GoodsReceivedLine_GoodsReceived_GoodsReceivedId",
+                table: "GoodsReceivedLine",
+                column: "GoodsReceivedId",
+                principalTable: "GoodsReceived",
+                principalColumn: "GoodsReceivedId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ControlPalletsLine_ControlPallets_ControlPalletsId",
+                table: "ControlPalletsLine");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_GoodsReceivedLine_GoodsReceived_GoodsReceivedId",
+                table: "GoodsReceivedLine");
+
+            migrationBuilder.DropIndex(
+                name: "IX_GoodsReceivedLine_GoodsReceivedId",
+                table: "GoodsReceivedLine");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ControlPalletsLine_ControlPalletsId",
+                table: "ControlPalletsLine");
+
+            migrationBuilder.DropColumn(
+                name: "WeightBallot",
+                table: "GoodsReceived");
+        }
+    }
+}
