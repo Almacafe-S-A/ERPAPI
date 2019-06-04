58868ec9b7ad9b89cc67a4e13ed0df5f1a9744bc
Freddy Chinchilla <fchinchillahn@gmail.com>
2019-06-04 10:12:13 -0600

    Numeracion SAR

--- ERPAPI/Controllers/GoodsReceivedController.cs
+++ ERPAPI/Controllers/GoodsReceivedController.cs
@@ -238,5 +238,6 @@ namespace ERPAPI.Controllers
 
 
 
+
     }
 }
--- ERPAPI/Models/NumeracionSAR.cs
+++ ERPAPI/Models/NumeracionSAR.cs
@@ -14,26 +14,21 @@ namespace ERPAPI.Models
         public Int64 IdNumeracion { get; set; }
 
         public Int64 IdCAI { get; set; }
-
         public string NoInicio { get; set; }
-
         public string NoFin { get; set; }
-
         public DateTime FechaLimite { get; set; }
-
         public int CantidadOtorgada { get; set; }
-
         public string SiguienteNumero { get; set; }
+        public Int64 IdPuntoEmision { get; set; }
         public string PuntoEmision { get; set; }
+        public Int64 DocTypeId { get; set; }
         public string DocType { get; set; }
+        public Int64 DocSubTypeId { get; set; }
         public string DocSubType { get; set; }
-
         [Display(Name = "Estado")]
         public Int64 IdEstado { get; set; }
-
         [Display(Name = "Estado")]
         public string Estado { get; set; }
-
         public DateTime FechaCreacion { get; set; }
         public DateTime FechaModificacion { get; set; }
         public string UsuarioCreacion { get; set; }
