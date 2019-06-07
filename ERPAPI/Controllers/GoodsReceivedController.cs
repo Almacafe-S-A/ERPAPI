1a87c794801370328412336627d4cd37b4a52e52
Freddy Chinchilla <fchinchillahn@gmail.com>
2019-06-06 19:54:28 -0600

    Recibos de mercaderia.

--- ERPAPI/Controllers/GoodsReceivedController.cs
+++ ERPAPI/Controllers/GoodsReceivedController.cs
@@ -234,11 +234,11 @@ namespace ERPAPI.Controllers
               //  List<GoodsReceivedLineDTO> d = new List<GoodsReceivedLineDTO>();
                 using (var command = _context.Database.GetDbConnection().CreateCommand())
                 {
-                    command.CommandText = ("  SELECT  grl.SubProductId, grl.SubProductName, grl.UnitOfMeasureName         "
+                    command.CommandText = ("  SELECT  grl.SubProductId,grl.UnitOfMeasureId, grl.SubProductName, grl.UnitOfMeasureName         "
                  + " , SUM(Quantity) AS Cantidad, SUM(grl.QuantitySacos) AS CantidadSacos         "
                  + "  , SUM(grl.Price) Precio, SUM(grl.Total) AS Total                            "
                  + $"  FROM GoodsReceivedLine grl                 where  GoodsReceivedId in ({inparams})                                "
-                 + "  GROUP BY grl.SubProductId, grl.SubProductName, grl.UnitOfMeasureName        "
+                 + "  GROUP BY grl.SubProductId,grl.UnitOfMeasureId, grl.SubProductName, grl.UnitOfMeasureName        "
                  );
 
                    _context.Database.OpenConnection();
@@ -247,13 +247,16 @@ namespace ERPAPI.Controllers
                         // do something with result
                         while (await result.ReadAsync())
                         {
-                            _goodsreceivedlis._GoodsReceivedLine.Add(new GoodsReceivedLine { SubProductId = Convert.ToInt64(result["SubProductId"]),
+                            _goodsreceivedlis._GoodsReceivedLine.Add(new GoodsReceivedLine {
+                                SubProductId = Convert.ToInt64(result["SubProductId"]),
                                 SubProductName = result["SubProductName"].ToString(),
+                                UnitOfMeasureId = Convert.ToInt64(result["UnitOfMeasureId"]),
                                 UnitOfMeasureName = result["UnitOfMeasureName"].ToString(),
                                 Quantity = Convert.ToInt32(result["Cantidad"]),
                                 QuantitySacos = Convert.ToInt32(result["CantidadSacos"]),
                                 Price = Convert.ToDouble(result["Precio"]),
                                 Total = Convert.ToDouble(result["Total"]),
+                                
                             });
                         }
                     }
