ef499e2edac95ada9f43901717bf2e7a710bd4c0
Freddy Chinchilla <fchinchillahn@gmail.com>
2019-07-22 17:23:46 -0600

    CompanyInfo_State_CONTROLLERS

---
+++ ERPAPI/Controllers/CompanyInfoController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using ERP.Contexts;
+using ERPAPI.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.HttpSys;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace ERPAPI.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("api/CompanyInfo")]
+    [ApiController]
+    public class CompanyInfoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger _logger;
+
+        public CompanyInfoController(ILogger<CompanyInfoController> logger, ApplicationDbContext context)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Obtiene el Listado de CompanyInfoes 
+        /// El estado define cuales son los cai activos
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetCompanyInfo()
+        {
+            List<CompanyInfo> Items = new List<CompanyInfo>();
+            try
+            {
+                Items = await _context.CompanyInfo.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError($"Ocurrio un error: { ex.ToString() }");
+                return BadRequest($"Ocurrio un error:{ex.Message}");
+            }
+
+            //  int Count = Items.Count();
+            return await Task.Run(() => Ok(Items));
+        }
+
+        /// <summary>
+        /// Obtiene los Datos de la CompanyInfo por medio del Id enviado.
+        /// </summary>
+        /// <param name="CompanyInfoId"></param>
+        /// <returns></returns>
+        [HttpGet("[action]/{CompanyInfoId}")]
+        public async Task<IActionResult> GetCompanyInfoById(Int64 CompanyInfoId)
+        {
+            CompanyInfo Items = new CompanyInfo();
+            try
+            {
+                Items = await _context.CompanyInfo.Where(q => q.CompanyInfoId == CompanyInfoId).FirstOrDefaultAsync();
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError($"Ocurrio un error: { ex.ToString() }");
+                return BadRequest($"Ocurrio un error:{ex.Message}");
+            }
+
+
+            return await Task.Run(() => Ok(Items));
+        }
+
+
+        /// <summary>
+        /// Inserta una nueva CompanyInfo
+        /// </summary>
+        /// <param name="_CompanyInfo"></param>
+        /// <returns></returns>
+        [HttpPost("[action]")]
+        public async Task<ActionResult<CompanyInfo>> Insert([FromBody]CompanyInfo _CompanyInfo)
+        {
+            CompanyInfo _CompanyInfoq = new CompanyInfo();
+            try
+            {
+                _CompanyInfoq = _CompanyInfo;
+                _context.CompanyInfo.Add(_CompanyInfoq);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError($"Ocurrio un error: { ex.ToString() }");
+                return BadRequest($"Ocurrio un error:{ex.Message}");
+            }
+
+            return await Task.Run(() => Ok(_CompanyInfoq));
+        }
+
+        /// <summary>
+        /// Actualiza la CompanyInfo
+        /// </summary>
+        /// <param name="_CompanyInfo"></param>
+        /// <returns></returns>
+        [HttpPut("[action]")]
+        public async Task<ActionResult<CompanyInfo>> Update([FromBody]CompanyInfo _CompanyInfo)
+        {
+            CompanyInfo _CompanyInfoq = _CompanyInfo;
+            try
+            {
+                _CompanyInfoq = await (from c in _context.CompanyInfo
+                                 .Where(q => q.CompanyInfoId == _CompanyInfo.CompanyInfoId)
+                                       select c
+                                ).FirstOrDefaultAsync();
+
+                _context.Entry(_CompanyInfoq).CurrentValues.SetValues((_CompanyInfo));
+
+                //_context.CompanyInfo.Update(_CompanyInfoq);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError($"Ocurrio un error: { ex.ToString() }");
+                return await Task.Run(() => BadRequest($"Ocurrio un error:{ex.Message}"));
+            }
+
+            return await Task.Run(() => Ok(_CompanyInfoq));
+        }
+
+        /// <summary>
+        /// Elimina una CompanyInfo       
+        /// </summary>
+        /// <param name="_CompanyInfo"></param>
+        /// <returns></returns>
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Delete([FromBody]CompanyInfo _CompanyInfo)
+        {
+            CompanyInfo _CompanyInfoq = new CompanyInfo();
+            try
+            {
+                _CompanyInfoq = _context.CompanyInfo
+                .Where(x => x.CompanyInfoId == (Int64)_CompanyInfo.CompanyInfoId)
+                .FirstOrDefault();
+
+                _context.CompanyInfo.Remove(_CompanyInfoq);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Ocurrio un error: { ex.ToString() }");
+                return await Task.Run(() => BadRequest($"Ocurrio un error:{ex.Message}"));
+            }
+
+            return await Task.Run(() => Ok(_CompanyInfoq));
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ ERPAPI/Controllers/StateController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using ERP.Contexts;
+using ERPAPI.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.HttpSys;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace ERPAPI.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("api/State")]
+    [ApiController]
+    public class StateController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger _logger;
+
+        public StateController(ILogger<StateController> logger, ApplicationDbContext context)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Obtiene el Listado de Statees 
+        /// El estado define cuales son los cai activos
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetState()
+        {
+            List<State> Items = new List<State>();
+            try
+            {
+                Items = await _context.State.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError($"Ocurrio un error: { ex.ToString() }");
+                return BadRequest($"Ocurrio un error:{ex.Message}");
+            }
+
+            //  int Count = Items.Count();
+            return await Task.Run(() => Ok(Items));
+        }
+
+        /// <summary>
+        /// Obtiene los Datos de la State por medio del Id enviado.
+        /// </summary>
+        /// <param name="StateId"></param>
+        /// <returns></returns>
+        [HttpGet("[action]/{StateId}")]
+        public async Task<IActionResult> GetStateById(Int64 StateId)
+        {
+            State Items = new State();
+            try
+            {
+                Items = await _context.State.Where(q => q.StateId == StateId).FirstOrDefaultAsync();
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError($"Ocurrio un error: { ex.ToString() }");
+                return BadRequest($"Ocurrio un error:{ex.Message}");
+            }
+
+
+            return await Task.Run(() => Ok(Items));
+        }
+
+
+        /// <summary>
+        /// Inserta una nueva State
+        /// </summary>
+        /// <param name="_State"></param>
+        /// <returns></returns>
+        [HttpPost("[action]")]
+        public async Task<ActionResult<State>> Insert([FromBody]State _State)
+        {
+            State _Stateq = new State();
+            try
+            {
+                _Stateq = _State;
+                _context.State.Add(_Stateq);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError($"Ocurrio un error: { ex.ToString() }");
+                return BadRequest($"Ocurrio un error:{ex.Message}");
+            }
+
+            return await Task.Run(() => Ok(_Stateq));
+        }
+
+        /// <summary>
+        /// Actualiza la State
+        /// </summary>
+        /// <param name="_State"></param>
+        /// <returns></returns>
+        [HttpPut("[action]")]
+        public async Task<ActionResult<State>> Update([FromBody]State _State)
+        {
+            State _Stateq = _State;
+            try
+            {
+                _Stateq = await (from c in _context.State
+                                 .Where(q => q.StateId == _State.StateId)
+                                 select c
+                                ).FirstOrDefaultAsync();
+
+                _context.Entry(_Stateq).CurrentValues.SetValues((_State));
+
+                //_context.State.Update(_Stateq);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError($"Ocurrio un error: { ex.ToString() }");
+                return await Task.Run(() => BadRequest($"Ocurrio un error:{ex.Message}"));
+            }
+
+            return await Task.Run(() => Ok(_Stateq));
+        }
+
+        /// <summary>
+        /// Elimina una State       
+        /// </summary>
+        /// <param name="_State"></param>
+        /// <returns></returns>
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Delete([FromBody]State _State)
+        {
+            State _Stateq = new State();
+            try
+            {
+                _Stateq = _context.State
+                .Where(x => x.StateId == (Int64)_State.StateId)
+                .FirstOrDefault();
+
+                _context.State.Remove(_Stateq);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Ocurrio un error: { ex.ToString() }");
+                return await Task.Run(() => BadRequest($"Ocurrio un error:{ex.Message}"));
+            }
+
+            return await Task.Run(() => Ok(_Stateq));
+
+        }
+
+
+
+
+
+
+
+    }
+}
