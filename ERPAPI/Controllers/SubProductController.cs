4dc6668c847282b26690ad4182cfcf6f70721468
Cristopher Arias <Ariasover@users.noreply.github.com>
2019-07-29 08:30:01 -0600

    Revert "CAMBIO 29/7"
    This reverts commit 10cc7de031d4a5f0c1a27a9b47143b1ca804582b.

--- ERPAPI/Controllers/SubProductController.cs
+++ ERPAPI/Controllers/SubProductController.cs
@@ -274,8 +274,8 @@ namespace ERPAPI.Controllers
                                           select c
                                     ).FirstOrDefault();
 
-                _subproduct.FechaModificacion = subproductq.FechaModificacion;
-                _subproduct.FechaModificacion = subproductq.FechaModificacion;
+                _subproduct.FechaCreacion = subproductq.FechaCreacion;
+                _subproduct.UsuarioCreacion = subproductq.UsuarioCreacion;
 
                 _context.Entry(subproductq).CurrentValues.SetValues((_subproduct));
                 //                _context.SubProduct.Update(_subproduct);
