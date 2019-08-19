2535f394c0479b2e682434e5a755314e979f0685
Cristopher Arias <ariasover@gmail.com>
2019-08-19 09:42:17 -0600

    Eliminar campo state id de City

--- ERPAPI/Migrations/20190819154050_borrar_estado.Designer.cs
+++ ERPAPI/Migrations/20190819154050_borrar_estado.Designer.cs
@@ -4,14 +4,16 @@ using ERP.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ERPAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190819154050_borrar_estado")]
+    partial class borrar_estado
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
