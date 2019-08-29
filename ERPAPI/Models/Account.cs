e60d4b6f1c2690c283aaf603a19c727eb335569d
Marvin Guillen <marvin.a.guillen@gmail.com>
2019-08-29 00:17:13 -0600

    Modified Account Description more lenght

--- ERPAPI/Models/Account.cs
+++ ERPAPI/Models/Account.cs
@@ -29,7 +29,8 @@ namespace ERPAPI.Models
         [StringLength(200)]
         [Display(Name = "Nombre de la cuenta")]
         public string AccountName { get; set; }
-        [StringLength(200)]
+
+        [MaxLength(5000)]
         [Display(Name = "Descripcion de la cuenta")]
         public string Description { get; set; }
         [Display(Name = "Estado")]
