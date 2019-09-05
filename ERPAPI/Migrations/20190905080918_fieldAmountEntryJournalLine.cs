75046dda39106e5f52178cd4a00c4ea5c07ed7fa
Marvin Guillen <marvin.a.guillen@gmail.com>
2019-09-05 02:50:05 -0600

    Changed of JournalEntry Line

---
+++ ERPAPI/Migrations/20190905080918_fieldAmountEntryJournalLine.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERPAPI.Migrations
+{
+    public partial class fieldAmountEntryJournalLine : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Amount",
+                table: "JournalEntryLine",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+           
+
+            
+
+           
+
+
+         
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+          /*  migrationBuilder.DropColumn(
+                name: "CityId",
+                table: "Customer");
+
+           
+            migrationBuilder.DropColumn(
+                name: "CountryName",
+                table: "Customer");
+
+            migrationBuilder.DropColumn(
+                name: "StateId",
+                table: "Customer");
+
+            migrationBuilder.DropColumn(
+                name: "CityId",
+                table: "Branch");
+
+            migrationBuilder.DropColumn(
+                name: "CountryId",
+                table: "Branch");
+
+            migrationBuilder.DropColumn(
+                name: "CountryName",
+                table: "Branch");
+
+            migrationBuilder.DropColumn(
+                name: "StateId",
+                table: "Branch");
+                */
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Amount",
+                table: "JournalEntryLine",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+    }
+}
