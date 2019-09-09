5264e43e620aa659eda56c4fbd88ee99d2f3da3f
Carlos Castillo <carlos.castillo@bi-dss.com>
2019-09-09 15:40:49 -0600

    Agregado Marca Lineas Grupos

---
+++ ERPAPI/Controllers/MarcaController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using ERP.Contexts;
+using ERPAPI.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace coderush.Controllers.Api
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("api/Marca")]
+    public class MarcaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger _logger;
+
+        public MarcaController(ILogger<MarcaController> logger, ApplicationDbContext context)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        // GET: api/Marca
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetMarca()
+        {
+            List<Marca> Items = new List<Marca>();
+            try
+            {
+                Items = await _context.Marcas.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError($"Ocurrio un error: { ex.ToString() }");
+                return BadRequest($"Ocurrio un error:{ex.Message}");
+            }
+
+            //  int Count = Items.Count();
+            return await Task.Run(() => Ok(Items));
+        }
+
+        // api/MarcaGetMarcaById
+        [HttpGet("[action]/{Id}")]
+        public async Task<IActionResult> GetMarcaById(int Id)
+        {
+            Marca Items = new Marca();
+            try
+            {
+                Items = await _context.Marcas.Where(q => q.MarcaId.Equals(Id)).FirstOrDefaultAsync();
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError($"Ocurrio un error: { ex.ToString() }");
+                return BadRequest($"Ocurrio un error:{ex.Message}");
+            }
+
+            return await Task.Run(() => Ok(Items));
+        }
+
+        [HttpPost("[action]")]
+        public async Task<ActionResult<Marca>> Insert([FromBody]Marca payload)
+        {
+            Marca Marca = payload;
+
+            try
+            {
+                _context.Marcas.Add(Marca);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError($"Ocurrio un error: { ex.ToString() }");
+                return BadRequest($"Ocurrio un error:{ex.Message}");
+            }
+
+            return await Task.Run(() => Ok(Marca));
+        }
+
+        [HttpPut("[action]")]
+        public async Task<ActionResult<Marca>> Update([FromBody]Marca _Marca)
+        {
+
+            try
+            {
+                Marca Marcaq = (from c in _context.Marcas
+                   .Where(q => q.MarcaId == _Marca.MarcaId)
+                                                select c
+                     ).FirstOrDefault();
+
+                _Marca.FechaCreacion = Marcaq.FechaCreacion;
+                _Marca.UsuarioCreacion = Marcaq.UsuarioCreacion;
+
+                _context.Entry(Marcaq).CurrentValues.SetValues((_Marca));
+                // _context.Marca.Update(_Marca);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError($"Ocurrio un error: { ex.ToString() }");
+                return await Task.Run(() => BadRequest($"Ocurrio un error:{ex.Message}"));
+            }
+
+            return await Task.Run(() => Ok(_Marca));
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Delete([FromBody]Marca payload)
+        {
+            Marca Marca = new Marca();
+            try
+            {
+                Marca = _context.Marcas
+                .Where(x => x.MarcaId == (int)payload.MarcaId)
+                .FirstOrDefault();
+                _context.Marcas.Remove(Marca);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError($"Ocurrio un error: { ex.ToString() }");
+                return await Task.Run(() => BadRequest($"Ocurrio un error:{ex.Message}"));
+            }
+
+            return await Task.Run(() => Ok(Marca));
+
+        }
+
+
+
+    }
+}
---
+++ ERPAPI/Migrations/20190909212234_LineaMarcaProducto.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERPAPI.Migrations
+{
+    public partial class LineaMarcaProducto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lineas",
+                columns: table => new
+                {
+                    LineaId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Descripcion = table.Column<string>(nullable: false),
+                    IdEstado = table.Column<long>(nullable: false),
+                    Estado = table.Column<string>(nullable: true),
+                    UsuarioCreacion = table.Column<string>(nullable: false),
+                    UsuarioModificacion = table.Column<string>(nullable: false),
+                    FechaCreacion = table.Column<DateTime>(nullable: false),
+                    FechaModificacion = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lineas", x => x.LineaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Marcas",
+                columns: table => new
+                {
+                    MarcaId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(nullable: false),
+                    IdEstado = table.Column<long>(nullable: false),
+                    Estado = table.Column<string>(nullable: true),
+                    UsuarioCreacion = table.Column<string>(nullable: false),
+                    UsuarioModificacion = table.Column<string>(nullable: false),
+                    FechaCreacion = table.Column<DateTime>(nullable: false),
+                    FechaModificacion = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Marcas", x => x.MarcaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Grupos",
+                columns: table => new
+                {
+                    GrupoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(nullable: false),
+                    IdEstado = table.Column<long>(nullable: false),
+                    Estado = table.Column<string>(nullable: true),
+                    LineaId = table.Column<int>(nullable: true),
+                    UsuarioCreacion = table.Column<string>(nullable: false),
+                    UsuarioModificacion = table.Column<string>(nullable: false),
+                    FechaCreacion = table.Column<DateTime>(nullable: false),
+                    FechaModificacion = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grupos", x => x.GrupoId);
+                    table.ForeignKey(
+                        name: "FK_Grupos_Lineas_LineaId",
+                        column: x => x.LineaId,
+                        principalTable: "Lineas",
+                        principalColumn: "LineaId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Grupos_LineaId",
+                table: "Grupos",
+                column: "LineaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Grupos");
+
+            migrationBuilder.DropTable(
+                name: "Marcas");
+
+            migrationBuilder.DropTable(
+                name: "Lineas");
+        }
+    }
+}
