b048454917b7e6ec560fb4496b065193470de5b8
Freddy Chinchilla <fchinchillahn@gmail.com>
2019-09-10 08:46:24 -0600

    MonedaTasaCambio

--- ERPAPI/Migrations/20190910144357_Moneda_ExchangeRate.Designer.cs
+++ ERPAPI/Migrations/20190910144357_Moneda_ExchangeRate.Designer.cs
@@ -4,14 +4,16 @@ using ERP.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ERPAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190910144357_Moneda_ExchangeRate")]
+    partial class Moneda_ExchangeRate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
