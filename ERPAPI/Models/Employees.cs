cfcf89618bcc15423a75c3561b9b93de647d9309
Osdy <osdi@hotmail.es>
2019-09-12 14:45:47 -0600

    Tipo Planillas Cambios a Empleados

--- ERPAPI/Models/Employees.cs
+++ ERPAPI/Models/Employees.cs
@@ -26,38 +26,37 @@ namespace ERPAPI.Models
 
         public long IdPuesto { get; set; }
         [ForeignKey("IdPuesto")]
-        public  Puesto Puesto { get; set; }
+        public Puesto Puesto { get; set; }
         public Int64 IdEstado { get; set; }
-        public  Estados Estados { get; set; }
+        public Estados Estados { get; set; }
 
         public long IdCity { get; set; }
         [ForeignKey("IdCity")]
-        public  City City { get; set; }
+        public City City { get; set; }
 
         public long IdState { get; set; }
         [ForeignKey("IdState")]
         public State State { get; set; }
         public long IdCountry { get; set; }
         [ForeignKey("IdCountry")]
-        public  Country Country { get; set; }
+        public Country Country { get; set; }
         public int IdCurrency { get; set; }
         [ForeignKey("IdCurrency")]
-        public  Currency Currency { get; set; }
-        public Guid ApplicationUserId { get; set; }
-        public  ApplicationUser ApplicationUser { get; set; }
-        public long IdEscala { get; set; }
-        [ForeignKey("IdEscala")]
-        public  Escala Escala { get; set; }
+        public Currency Currency { get; set; }
+
+        public Guid? ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
         public Int64 IdBank { get; set; }
         [ForeignKey("IdBank")]
-        public  Bank Bank { get; set; }        
+        public Bank Bank { get; set; }
         public int IdBranch { get; set; }
         [ForeignKey("IdBranch")]
-        public  Branch Branch { get; set; }
+        public Branch Branch { get; set; }
         public long IdTipoContrato { get; set; }
         [ForeignKey("IdTipoContrato")]
-        public  TipoContrato TipoContrato { get; set; }
-        public  long IdDepartamento { get; set; }
+        public TipoContrato TipoContrato { get; set; }
+        public long IdDepartamento { get; set; }
         [ForeignKey("IdDepartamento")]
         public Departamento Departamento { get; set; }
 
@@ -71,13 +70,12 @@ namespace ERPAPI.Models
         public string TipoSangre { get; set; }
         public string NombreContacto { get; set; }
         public string TelefonoContacto { get; set; }
-        public int IdPlanilla { get; set; }
 
         public string Usuariocreacion { get; set; }
-        public string Usuariomodificacion { get; set; } 
+        public string Usuariomodificacion { get; set; }
         public DateTime? FechaCreacion { get; set; }
         public DateTime? FechaModificacion { get; set; }
-       // [Display(Name = "Party")]
+        // [Display(Name = "Party")]
         //public  Party Party { get; set; }
 
         //public int? PartyId { get; set; }
