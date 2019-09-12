63db3baaa61d753964d04908b7c111d12f6a32e1
Osdy <osdi@hotmail.es>
2019-09-12 11:31:21 -0600

    Nuevos campos en Dependientes

---
+++ ERPAPI/Migrations/20190912150235_Dependientes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERPAPI.Migrations
+{
+    public partial class Dependientes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Edad",
+                table: "Dependientes",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaNacimiento",
+                table: "Dependientes",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Edad",
+                table: "Dependientes");
+
+            migrationBuilder.DropColumn(
+                name: "FechaNacimiento",
+                table: "Dependientes");
+        }
+    }
+}
