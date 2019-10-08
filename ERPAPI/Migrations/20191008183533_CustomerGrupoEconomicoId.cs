2a02880c016d89e3fc4be2794953771fe2e8e77d
Freddy Chinchilla <fchinchillahn@gmail.com>
2019-10-08 12:36:25 -0600

    Grupo economico id

---
+++ ERPAPI/Migrations/20191008183533_CustomerGrupoEconomicoId.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERPAPI.Migrations
+{
+    public partial class CustomerGrupoEconomicoId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "GrupoEconomicoId",
+                table: "Customer",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GrupoEconomicoId",
+                table: "Customer");
+        }
+    }
+}
