abeabbda6ff21b4e1fcb82b1a127223598899109
Freddy Chinchilla <fchinchillahn@gmail.com>
2019-10-12 01:05:00 -0600

    Invoice

--- ERPAPI/Migrations/20191012054142_SubProduct_TipoProhibicion.Designer.cs
+++ ERPAPI/Migrations/20191012054142_SubProduct_TipoProhibicion.Designer.cs
@@ -4,14 +4,16 @@ using ERP.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ERPAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191012054142_SubProduct_TipoProhibicion")]
+    partial class SubProduct_TipoProhibicion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
