0d4dac3e56a4dacd6d5a22e641dae9a5e0820e28
Freddy Chinchilla <fchinchillahn@gmail.com>
2019-10-16 20:15:19 -0600

    LogicalCondition

--- ERPAPI/Migrations/20191017021020_CustomerConditions_LogicalCondition.Designer.cs
+++ ERPAPI/Migrations/20191017021020_CustomerConditions_LogicalCondition.Designer.cs
@@ -4,14 +4,16 @@ using ERP.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ERPAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191017021020_CustomerConditions_LogicalCondition")]
+    partial class CustomerConditions_LogicalCondition
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
