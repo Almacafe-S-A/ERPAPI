e06d46d5cb9b02beedae9959e30438747b0fc05d
Carlos Castillo <carlos.castillo@bi-dss.com>
2019-10-18 11:55:32 -0600

    VendorInvoice

--- ERPAPI/Migrations/20191018175412_VendorInvoice-RemoveFields.Designer.cs
+++ ERPAPI/Migrations/20191018175412_VendorInvoice-RemoveFields.Designer.cs
@@ -4,14 +4,16 @@ using ERP.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ERPAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191018175412_VendorInvoice-RemoveFields")]
+    partial class VendorInvoiceRemoveFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
