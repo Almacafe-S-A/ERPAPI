13935bb77a4ab39b753e5a748ea14919bf145c47
Brayan Interiano <44504858+binteriano0502@users.noreply.github.com>
2019-11-11 16:48:32 -0600

    Save, CreditNote y DebitNote

--- ERPAPI/Controllers/CreditNoteController.cs
+++ ERPAPI/Controllers/CreditNoteController.cs
@@ -126,36 +126,37 @@ namespace ERPAPI.Controllers
                     try
                     {
                         _CreditNoteq = _CreditNote;
-
-                        CreditNote _creditnote = await _context.CreditNote.Where(q => q.BranchId == _CreditNote.BranchId)
-                                             .Where(q => q.IdPuntoEmision == _CreditNote.IdPuntoEmision)
-                                             .FirstOrDefaultAsync();
-                        if (_creditnote != null)
+                        if (!_CreditNote.Fiscal)
                         {
-                            _CreditNoteq.NúmeroDEI = _context.CreditNote.Where(q => q.BranchId == _CreditNote.BranchId)
-                                                  .Where(q => q.IdPuntoEmision == _CreditNote.IdPuntoEmision).Max(q => q.NúmeroDEI);
+                            CreditNote _creditnote = await _context.CreditNote.Where(q => q.BranchId == _CreditNote.BranchId)
+                                                 .Where(q => q.IdPuntoEmision == _CreditNote.IdPuntoEmision)
+                                                 .FirstOrDefaultAsync();
+                            if (_creditnote != null)
+                            {
+                                _CreditNoteq.NúmeroDEI = _context.CreditNote.Where(q => q.BranchId == _CreditNote.BranchId)
+                                                      .Where(q => q.IdPuntoEmision == _CreditNote.IdPuntoEmision).Max(q => q.NúmeroDEI);
+                            }
+
+                            _CreditNoteq.NúmeroDEI += 1;
+
+
+                            //  Int64 puntoemision = _context.Users.Where(q=>q.Email==_Invoiceq.UsuarioCreacion).Select(q=>q.)
+
+                            Int64 IdCai = await _context.NumeracionSAR
+                                                     .Where(q => q.BranchId == _CreditNoteq.BranchId)
+                                                     .Where(q => q.IdPuntoEmision == _CreditNoteq.IdPuntoEmision)
+                                                     .Where(q => q.Estado == "Activo").Select(q => q.IdCAI).FirstOrDefaultAsync();
+
+
+                            if (IdCai == 0)
+                            {
+                                return BadRequest("No existe un CAI activo para el punto de emisión");
+                            }
+
+                            _CreditNoteq.Sucursal = await _context.Branch.Where(q => q.BranchId == _CreditNote.BranchId).Select(q => q.BranchCode).FirstOrDefaultAsync();
+                            //  _Invoiceq.Caja = await _context.PuntoEmision.Where(q=>q.IdPuntoEmision== _Invoice.IdPuntoEmision).Select(q => q.PuntoEmisionCod).FirstOrDefaultAsync();
+                            _CreditNoteq.CAI = await _context.CAI.Where(q => q.IdCAI == IdCai).Select(q => q._cai).FirstOrDefaultAsync();
                         }
-
-                        _CreditNoteq.NúmeroDEI += 1;
-
-
-                        //  Int64 puntoemision = _context.Users.Where(q=>q.Email==_Invoiceq.UsuarioCreacion).Select(q=>q.)
-
-                        Int64 IdCai = await _context.NumeracionSAR
-                                                 .Where(q => q.BranchId == _CreditNoteq.BranchId)
-                                                 .Where(q => q.IdPuntoEmision == _CreditNoteq.IdPuntoEmision)
-                                                 .Where(q => q.Estado == "Activo").Select(q => q.IdCAI).FirstOrDefaultAsync();
-
-
-                        if (IdCai == 0)
-                        {
-                            return BadRequest("No existe un CAI activo para el punto de emisión");
-                        }
-
-                        _CreditNoteq.Sucursal = await _context.Branch.Where(q => q.BranchId == _CreditNote.BranchId).Select(q => q.BranchCode).FirstOrDefaultAsync();
-                        //  _Invoiceq.Caja = await _context.PuntoEmision.Where(q=>q.IdPuntoEmision== _Invoice.IdPuntoEmision).Select(q => q.PuntoEmisionCod).FirstOrDefaultAsync();
-                        _CreditNoteq.CAI = await _context.CAI.Where(q => q.IdCAI == IdCai).Select(q => q._cai).FirstOrDefaultAsync();
-
                         Numalet let;
                         let = new Numalet();
                         let.SeparadorDecimalSalida = "Lempiras";
