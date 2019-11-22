631bcdc84dad620df63f4bb16069193de7ddc2e9
Marvin Guillen <marvin.a.guillen@gmail.com>
2019-11-22 08:55:21 -0600

    Changes of Migration ApplicationClaimTable to comment

--- ERPAPI/Migrations/20191121204717_ApplicationUserClaimTable.cs
+++ ERPAPI/Migrations/20191121204717_ApplicationUserClaimTable.cs
@@ -6,45 +6,45 @@ namespace ERPAPI.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_EmployeeExtraHours_Customer_CustomerId",
-                table: "EmployeeExtraHours");
+            /* migrationBuilder.DropForeignKey(
+                 name: "FK_EmployeeExtraHours_Customer_CustomerId",
+                 table: "EmployeeExtraHours");
 
-            migrationBuilder.AlterColumn<long>(
-                name: "CustomerId",
-                table: "EmployeeExtraHours",
-                nullable: true,
-                oldClrType: typeof(long));
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_EmployeeExtraHours_Customer_CustomerId",
-                table: "EmployeeExtraHours",
-                column: "CustomerId",
-                principalTable: "Customer",
-                principalColumn: "CustomerId",
-                onDelete: ReferentialAction.Restrict);
+             migrationBuilder.AlterColumn<long>(
+                 name: "CustomerId",
+                 table: "EmployeeExtraHours",
+                 nullable: true,
+                 oldClrType: typeof(long));
+                 */
+            /* migrationBuilder.AddForeignKey(
+                 name: "FK_EmployeeExtraHours_Customer_CustomerId",
+                 table: "EmployeeExtraHours",
+                 column: "CustomerId",
+                 principalTable: "Customer",
+                 principalColumn: "CustomerId",
+                 onDelete: ReferentialAction.NoAction);*/
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_EmployeeExtraHours_Customer_CustomerId",
-                table: "EmployeeExtraHours");
+            /*  migrationBuilder.DropForeignKey(
+                  name: "FK_EmployeeExtraHours_Customer_CustomerId",
+                  table: "EmployeeExtraHours");
 
-            migrationBuilder.AlterColumn<long>(
-                name: "CustomerId",
-                table: "EmployeeExtraHours",
-                nullable: false,
-                oldClrType: typeof(long),
-                oldNullable: true);
+              migrationBuilder.AlterColumn<long>(
+                  name: "CustomerId",
+                  table: "EmployeeExtraHours",
+                  nullable: false,
+                  oldClrType: typeof(long),
+                  oldNullable: true);
 
-            migrationBuilder.AddForeignKey(
-                name: "FK_EmployeeExtraHours_Customer_CustomerId",
-                table: "EmployeeExtraHours",
-                column: "CustomerId",
-                principalTable: "Customer",
-                principalColumn: "CustomerId",
-                onDelete: ReferentialAction.Cascade);
+              migrationBuilder.AddForeignKey(
+                  name: "FK_EmployeeExtraHours_Customer_CustomerId",
+                  table: "EmployeeExtraHours",
+                  column: "CustomerId",
+                  principalTable: "Customer",
+                  principalColumn: "CustomerId",
+                  onDelete: ReferentialAction.Cascade);*/
         }
     }
 }
