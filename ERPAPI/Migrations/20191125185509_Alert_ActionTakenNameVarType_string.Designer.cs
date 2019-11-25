bdc39b83671e08af1f4fd9abd572505e3b91249b
Carlos Eduardo Castillo <carlos.castillo@bi-dss..com>
2019-11-25 13:03:38 -0600

    Cambio del tipo de dato de ActionTaken  de int a string

--- ERPAPI/Migrations/20191125185509_Alert_ActionTakenNameVarType_string.Designer.cs
+++ ERPAPI/Migrations/20191125185509_Alert_ActionTakenNameVarType_string.Designer.cs
@@ -4,14 +4,16 @@ using ERP.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ERPAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191125185509_Alert_ActionTakenNameVarType_string")]
+    partial class Alert_ActionTakenNameVarType_string
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
