9e26f411e4a057f70fd2ed449c1ca8ba400d37c7
Carlos Eduardo Castillo <carlos.castillo@bi-dss..com>
2019-11-26 21:02:29 -0600

    agregado Campo observacion a Alertas

--- ERPAPI/Migrations/20191126195711_AddField_Observacion_Alert.Designer.cs
+++ ERPAPI/Migrations/20191126195711_AddField_Observacion_Alert.Designer.cs
@@ -4,14 +4,16 @@ using ERP.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ERPAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191126195711_AddField_Observacion_Alert")]
+    partial class AddField_Observacion_Alert
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ERPAPI/Models/Alert.cs
+++ ERPAPI/Models/Alert.cs
@@ -52,6 +52,8 @@ namespace ERPAPI.Models
 
         public string AlertType { get; set; } /// PERSONAS, PRODUCTO
 
+        public string Observacion { get; set; }
+
         public DateTime FechaCreacion { get; set; }
         public DateTime FechaModificacion { get; set; }
         public string UsuarioCreacion { get; set; }
