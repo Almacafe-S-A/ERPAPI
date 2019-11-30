c9db3bae77d2a38474a9333c7b3dd363b50eb915
csolorzanof <carlos.solorzano@bi-dss.com>
2019-11-29 20:58:51 -0600

    Migracion creada para agregar campos adicionales a la tabla de cuentas

---
+++ ERPAPI/Migrations/20191129221656_CamposAdicionalesCuentas.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERPAPI.Migrations
+{
+    public partial class CamposAdicionalesCuentas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name:"Mayoriza", 
+                nullable:false,
+                defaultValue:false,
+                table: "Accounting"
+            );
+
+            migrationBuilder.AddColumn<string>(
+                name: "NaturalezaCuenta",
+                nullable: false,
+                defaultValue: "D",
+                table: "Accounting"
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "Mayoriza", table: "Accounting");
+            migrationBuilder.DropColumn("NaturalezaCuenta", table: "Accounting");
+        }
+    }
+}
