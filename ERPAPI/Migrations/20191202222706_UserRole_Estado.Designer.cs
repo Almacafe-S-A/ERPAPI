d9f713d887f0ba1dd6e95cb970b746d116cdf3bb
Alfredo Matute Ochoa <pidgy@hotmail.es>
2019-12-02 16:43:59 -0600

    UserRole - Se agrego estado

--- ERPAPI/Migrations/20191202222706_UserRole_Estado.Designer.cs
+++ ERPAPI/Migrations/20191202222706_UserRole_Estado.Designer.cs
@@ -4,14 +4,16 @@ using ERP.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ERPAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191202222706_UserRole_Estado")]
+    partial class UserRole_Estado
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
