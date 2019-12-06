2a4ed14b9610400338a706f81b861bea5b961503
Marvin Guillen <marvin.a.guillen@gmail.com>
2019-12-06 00:42:54 -0600

    Correccion por mala aplicacion de fusion de rama Changes of tipos de comision Marvin Guillen

--- ERPAPI/Controllers/TypeJournalController.cs
+++ ERPAPI/Controllers/TypeJournalController.cs
@@ -135,30 +135,6 @@ namespace ERPAPI.Controllers
             return await Task.Run(() => Ok(Items));
         }
 
-
-        /// <summary>
-        /// Obtiene los Datos de la Journal por medio del Id enviado.
-        /// </summary>
-        /// <param name="TypeJournalName"></param>
-        /// <returns></returns>
-        [HttpGet("[action]/{TypeJournalName}")]
-        public async Task<IActionResult> GetTypeJournalByName(String TypeJournalName)
-        {
-            TypeJournal Items = new TypeJournal();
-            try
-            {
-                Items = await _context.TypeJournal.Where(q => q.TypeJournalName == TypeJournalName).FirstOrDefaultAsync();
-            }
-            catch (Exception ex)
-            {
-
-                _logger.LogError($"Ocurrio un error: { ex.ToString() }");
-                return BadRequest($"Ocurrio un error:{ex.Message}");
-            }
-
-
-            return await Task.Run(() => Ok(Items));
-        }
     
 
     /// <summary>
