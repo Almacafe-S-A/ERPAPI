333066846b9ec8bf2673a2d3644d974d54904f00
Carlos Eduardo Castillo <carlos.castillo@bi-dss..com>
2019-12-16 12:15:53 -0600

    Campos de Auditoria

--- ERPAPI/Models/CheckAccountLines.cs
+++ ERPAPI/Models/CheckAccountLines.cs
@@ -29,5 +29,10 @@ namespace ERPAPI.Models
         public string AmountWords { get; set; }
 
         public string Address { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public string UsuarioCreacion { get; set; }
+        public string UsuarioModificacion { get; set; }
     }
 }
