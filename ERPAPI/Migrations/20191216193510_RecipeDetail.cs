2fe44f5c3529c8b09e55b3c4eae6a5765349820b
Alfredo Matute Ochoa <pidgy@hotmail.es>
2019-12-17 11:26:58 -0600

    Detalle Receta - se le quito cualquier referencia de tablas que no sean RecipeDetail a la migracion

--- ERPAPI/Migrations/20191216193510_RecipeDetail.cs
+++ ERPAPI/Migrations/20191216193510_RecipeDetail.cs
@@ -9,45 +9,6 @@ namespace ERPAPI.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "Material",
-                columns: table => new
-                {
-                    MaterialId = table.Column<long>(nullable: false)
-                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    Description = table.Column<string>(nullable: true),
-                    MaterialCode = table.Column<string>(nullable: true),
-                    MaterialType = table.Column<string>(nullable: true),
-                    FechaCreacion = table.Column<DateTime>(nullable: false),
-                    FechaModificacion = table.Column<DateTime>(nullable: false),
-                    UsuarioModificacion = table.Column<string>(nullable: true),
-                    UsuarioCreacion = table.Column<string>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Material", x => x.MaterialId);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "Recipe",
-                columns: table => new
-                {
-                    RecipeId = table.Column<long>(nullable: false)
-                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
-                    RecipeCode = table.Column<string>(nullable: true),
-                    Description = table.Column<string>(nullable: true),
-                    EstatusId = table.Column<long>(nullable: false),
-                    Estatus = table.Column<string>(nullable: true),
-                    FechaCreacion = table.Column<DateTime>(nullable: false),
-                    FechaModificacion = table.Column<DateTime>(nullable: false),
-                    UsuarioModificacion = table.Column<string>(nullable: true),
-                    UsuarioCreacion = table.Column<string>(nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Recipe", x => x.RecipeId);
-                });
-
-            migrationBuilder.CreateTable(
                 name: "RecipeDetail",
                 columns: table => new
                 {
@@ -88,16 +49,7 @@ namespace ERPAPI.Migrations
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "InstallmentDelivery");
-
-            migrationBuilder.DropTable(
-                name: "Material");
-
-            migrationBuilder.DropTable(
                 name: "RecipeDetail");
-
-            migrationBuilder.DropTable(
-                name: "Recipe");
         }
     }
 }
