a23a78a00fc7b24bf7241069d7f3aa66ae6f27f0
Carlos Castillo <carlos.castillo@bi-dss.com>
2019-12-22 15:17:26 -0600

    Correcion Llave Tpod de Documento

--- ERPAPI/Migrations/20191222211327_CorrecionFK_TipoDocumento.Designer.cs
+++ ERPAPI/Migrations/20191222211327_CorrecionFK_TipoDocumento.Designer.cs
@@ -4,14 +4,16 @@ using ERP.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ERPAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191222211327_CorrecionFK_TipoDocumento")]
+    partial class CorrecionFK_TipoDocumento
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
