920086a9ccd36f5109f0248af98e72e706dcb57f
Carlos Castillo <carlos.castillo@bi-dss.com>
2020-01-07 12:05:57 -0600

    Agregados campos a Journal Entry

--- ERPAPI/Migrations/20200107180232_Added_Fields_journal_Entry.Designer.cs
+++ ERPAPI/Migrations/20200107180232_Added_Fields_journal_Entry.Designer.cs
@@ -4,14 +4,16 @@ using ERP.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ERPAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200107180232_Added_Fields_journal_Entry")]
+    partial class Added_Fields_journal_Entry
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ERPAPI/Migrations/20200107180232_Added_Fields_journal_Entry.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERPAPI.Migrations
+{
+    public partial class Added_Fields_journal_Entry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApprovedBy",
+                table: "JournalEntry",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ApprovedDate",
+                table: "JournalEntry",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PartyName",
+                table: "JournalEntry",
+                nullable: true);
+
+           
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApprovedBy",
+                table: "JournalEntry");
+
+            migrationBuilder.DropColumn(
+                name: "ApprovedDate",
+                table: "JournalEntry");
+
+            migrationBuilder.DropColumn(
+                name: "PartyName",
+                table: "JournalEntry");
+        }
+    }
+}
