b54fb377f3659fc461b22880c67560e2bc5ddb0c
Carlos Castillo <carlos.castillo@bi-dss.com>
2020-01-20 23:12:24 -0600

    Agrregado Campo de Tasa de Compra

--- ERPAPI/Migrations/20200121050659_AddedField_ExhangeRateCompra.Designer.cs
+++ ERPAPI/Migrations/20200121050659_AddedField_ExhangeRateCompra.Designer.cs
@@ -4,14 +4,16 @@ using ERP.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ERPAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200121050659_AddedField_ExhangeRateCompra")]
+    partial class AddedField_ExhangeRateCompra
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -4210,7 +4212,7 @@ namespace ERPAPI.Migrations
                     b.Property<decimal>("ExchangeRateValue")
                         .HasColumnType("decimal(18,4)");
 
-                    b.Property<decimal?>("ExchangeRateValueCompra")
+                    b.Property<decimal>("ExchangeRateValueCompra")
                         .HasColumnType("decimal(18,4)");
 
                     b.Property<DateTime>("ModifiedDate");
---
+++ ERPAPI/Migrations/20200121050659_AddedField_ExhangeRateCompra.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERPAPI.Migrations
+{
+    public partial class AddedField_ExhangeRateCompra : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ExchangeRateValueCompra",
+                table: "ExchangeRate",
+                type: "decimal(18,4)",
+                nullable: false,
+                defaultValue: 0m);
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+
+            migrationBuilder.DropColumn(
+                name: "ExchangeRateValueCompra",
+                table: "ExchangeRate");
+
+          
+        }
+    }
+}
