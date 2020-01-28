70ee3eb44d20fd58150fa8f57ec6e24a1f65b192
Carlos Castillo <carlos.castillo@bi-dss.com>
2020-01-27 21:23:36 -0600

    Campos Agregados a Cierres

--- ERPAPI/Migrations/20200128032234_AddedFields_CierresJournalEntries.Designer.cs
+++ ERPAPI/Migrations/20200128032234_AddedFields_CierresJournalEntries.Designer.cs
@@ -4,14 +4,16 @@ using ERP.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ERPAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200128032234_AddedFields_CierresJournalEntries")]
+    partial class AddedFields_CierresJournalEntries
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ERPAPI/Migrations/20200128032234_AddedFields_CierresJournalEntries.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERPAPI.Migrations
+{
+    public partial class AddedFields_CierresJournalEntries : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BitacoraCierreContableId",
+                table: "CierresJournalEntryLine",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaCierre",
+                table: "CierresJournalEntryLine",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApprovedBy",
+                table: "CierresJournal",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CierresJournalEntryLine_BitacoraCierreContableId",
+                table: "CierresJournalEntryLine",
+                column: "BitacoraCierreContableId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CierresJournalEntryLine_BitacoraCierreContable_BitacoraCierreContableId",
+                table: "CierresJournalEntryLine",
+                column: "BitacoraCierreContableId",
+                principalTable: "BitacoraCierreContable",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CierresJournalEntryLine_BitacoraCierreContable_BitacoraCierreContableId",
+                table: "CierresJournalEntryLine");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CierresJournalEntryLine_BitacoraCierreContableId",
+                table: "CierresJournalEntryLine");
+
+            migrationBuilder.DropColumn(
+                name: "BitacoraCierreContableId",
+                table: "CierresJournalEntryLine");
+
+            migrationBuilder.DropColumn(
+                name: "FechaCierre",
+                table: "CierresJournalEntryLine");
+
+            migrationBuilder.DropColumn(
+                name: "ApprovedBy",
+                table: "CierresJournal");
+        }
+    }
+}
