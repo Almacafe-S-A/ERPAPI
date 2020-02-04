ace99782a561712abf5d5949e847b5890c5c4d45
Carlos Castillo <carlos.castillo@bi-dss.com>
2020-02-04 08:35:52 -0600

    Modificaciones a la Tabla de Horas Extras Empleados

--- ERPAPI/Models/EmployeeExtraHours.cs
+++ ERPAPI/Models/EmployeeExtraHours.cs
@@ -16,6 +16,9 @@ namespace ERPAPI.Models
         [Display(Name = "Id Empleado")]
         public Int64 EmployeeId { get; set; }
 
+        [ForeignKey("EmployeeId")]
+        public Employees Employees { get; set; }
+
         [Display(Name = "Empleado")]
         public string EmployeeName { get; set; }
 
@@ -25,10 +28,25 @@ namespace ERPAPI.Models
         [Display(Name = "Motivo")]
         public string Motivo { get; set; }
 
-        //public Customer Customer { get; set; }
-        //[Display(Name = "Id Cliente")]
-        //[ForeignKey("CustomerId")]
-        //public Int64? CustomerId { get; set; }
+        [Display(Name = "Cliente")]
+        public Int64 CustomerId { get; set; }
+        [ForeignKey("CustomerId")]
+        public Customer Customer { get; set; }
+
+        [Display(Name = "Cliente")]
+        public string CustomerName { get; set; }
+
+        [Display(Name = "Hora de inicio")]
+        public DateTime StartTime { get; set; }
+
+        [Display(Name = "Hora de fin")]
+        public DateTime EndTime { get; set; }
+
+        [Display(Name = "Cantidad de horas")]
+        public decimal QuantityHours { get; set; }
+
+        [Display(Name = "Factor Salario")]
+        public decimal HourlySalary { get; set; }
 
         [Display(Name = "Usuario modificación")]
         public string UsuarioModificacion { get; set; }
@@ -43,6 +61,6 @@ namespace ERPAPI.Models
         public DateTime FechaModificacion { get; set; }
 
      
-        public List<EmployeeExtraHoursDetail> EmployeeExtraHoursDetail { get; set; }
+       // public List<EmployeeExtraHoursDetail> EmployeeExtraHoursDetail { get; set; }
     }
 }
