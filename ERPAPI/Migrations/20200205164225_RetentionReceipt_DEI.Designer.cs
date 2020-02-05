ef4eaf94061b2253c02e5b21b01dd71248ee4902
Alfredo Matute Ochoa <pidgy@hotmail.es>
2020-02-05 11:12:14 -0600

    RetentionReceipt - cambios en modelo y migracion para agregar campos

--- ERPAPI/Migrations/20200205164225_RetentionReceipt_DEI.Designer.cs
+++ ERPAPI/Migrations/20200205164225_RetentionReceipt_DEI.Designer.cs
@@ -4,14 +4,16 @@ using ERP.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ERPAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200205164225_RetentionReceipt_DEI")]
+    partial class RetentionReceipt_DEI
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
