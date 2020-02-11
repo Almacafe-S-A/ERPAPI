77dc6eeab7bfaeac9d76206f0b6a59692d4fb2b1
Alfredo Matute Ochoa <pidgy@hotmail.es>
2020-02-11 16:37:46 -0600

    Correcciones al query generar balance comparativo

--- ERPAPI/Query/11- SPROC - Generar Balance Comparativo.sql
+++ ERPAPI/Query/11- SPROC - Generar Balance Comparativo.sql
@@ -1,16 +1,12 @@
-
-
-/****** Object:  StoredProcedure [dbo].[GenerarBalanceComparativo2]    Script Date: 6/2/2020 14:00:08 ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
 
 
 
 
-CREATE OR ALTER  PROCEDURE [dbo].[GenerarBalanceComparativo2]
+CREATE OR ALTER    PROCEDURE [dbo].[GenerarBalanceComparativo2]
 	@ANIO INT,	
 	@NIVEL INT,
 	@CENTROCOSTO BIGINT
@@ -381,20 +377,8 @@ BEGIN
 	WHEN B.DeudoraAcreedora = 'A' THEN '2'
 	ELSE '1'
 	END AS 'DeudoraAcreedora', B.Estado, B.Totaliza, 
-	--CASE T2.AceptaNegativo
-	--WHEN 1 THEN
-	--	Round(B.SaldoPrevAnio,2) * -1
-	--ELSE
-	--	Round(B.SaldoPrevAnio,2)
-	--END
 	Round(B.SaldoPrevAnio,2) AS 'AñoAnterior', 
 	Round(B.SaldoPrev,2) SaldoPrev, Round(B.Debe,2) Debe, Round(B.Haber,2) Haber, 
-	--CASE T2.AceptaNegativo
-	--WHEN 1 THEN
-	--	Round(B.SaldoFinal,2) * -1
-	--ELSE
-	--	Round(B.SaldoFinal,2)
-	--END
 	Round(B.SaldoFinal,2) AS 'AñoActual'
 	, T2.TypeAccountId, T3.TypeAccountName AS 'TipoDeCuenta'
 	, T4.ParentAccountId AS 'SubCuentaId'
@@ -412,7 +396,7 @@ BEGIN
 	AND NOT (B.HierarchyAccount > 2 AND T2.TypeAccountId = 4)
 	ORDER BY B.AccountCode
 
-	DELETE FROM #BalanceComparativo WHERE AccountCode = 58 OR AccountCode = 68
+	DELETE FROM #BalanceComparativo WHERE AccountCode = '58' OR AccountCode = '68'
 	DELETE FROM #BalanceComparativo WHERE Nivel = 1
 
 	---REINICIAR EL CONTEO DE COLUMNAS
@@ -431,12 +415,12 @@ BEGIN
 
 	UPDATE #BalanceComparativo
 	SET Columna = Columna + 1
-	WHERE (CAST(SUBSTRING(AccountCode, 1, 2) AS BIGINT) > = 54)
+	WHERE (CONVERT(BIGINT, (SUBSTRING(AccountCode, 1, 2))) > = 54)
 
 	SET @id = (SELECT Columna FROM #BalanceComparativo WHERE AccountCode = '54') - 1
 	UPDATE #BalanceComparativo
 	SET @id = Columna = @id + 1
-	WHERE (CAST(SUBSTRING(AccountCode, 1, 2) AS BIGINT) BETWEEN 61 AND 63)
+	WHERE (CONVERT(BIGINT, (SUBSTRING(AccountCode, 1, 2))) BETWEEN 61 AND 63)
 
 	SET @id = @id + 1
 	INSERT INTO #BalanceComparativo 
@@ -545,6 +529,3 @@ BEGIN
 	SELECT * FROM #BalanceComparativo T0
 	ORDER BY T0.Columna
 END											
-GO
-
-
