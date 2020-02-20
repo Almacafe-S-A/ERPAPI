6fe94f43b6d194aeb65f3cb16a4f921f2a49fe1a
Carlos Castillo <carlos.castillo@bi-dss.com>
2020-02-19 21:12:39 -0600

    Agregado campo de horas Extras a Empleado

---
+++ ERPAPI/Migrations/20200220031037_AddedFieldEmployeesExtrahour.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERPAPI.Migrations
+{
+    public partial class AddedFieldEmployeesExtrahour : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HorasExtra",
+                table: "Employees",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HorasExtra",
+                table: "Employees");
+        }
+    }
+}
