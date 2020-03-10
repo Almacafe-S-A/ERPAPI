3d183e8827defcf68455094bf800298b8b57848d
Alfredo Matute Ochoa <pidgy@hotmail.es>
2020-03-10 16:57:20 -0600

    Cambios en el numero de nota segun lo solicitado por el cliente

--- ERPAPI/Query/18 - SPROC - ListaNotasEstadosFinancieros.sql
+++ ERPAPI/Query/18 - SPROC - ListaNotasEstadosFinancieros.sql
@@ -1,6 +1,5 @@
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
 
@@ -451,6 +450,36 @@ BEGIN
 		UPDATE #BalanceSaldo SET SaldoFinal = SaldoFinal + (@Ingresos - @Gastos) WHERE AccountCode = '58';		
 	END;
 
+	--INSERT INTO #NotasEstadosFinancieros
+	--SELECT 
+	--Nota = CASE WHEN B.HierarchyAccount != 2 THEN NULL ELSE ROW_NUMBER() OVER (PARTITION BY CASE WHEN B.HierarchyAccount != 2 THEN 1 ELSE 0 END ORDER BY B.AccountId) END + 3,
+	--B.AccountId, B.AccountCode, B.AccountName AS 'Descripcion', B.ParentAccountId, 
+	--CASE
+	--WHEN B.DeudoraAcreedora = 'A' THEN '2'
+	--ELSE '1'
+	--END AS 'DeudoraAcreedora', B.Estado, B.Totaliza, 
+	--Round(B.SaldoPrevAnio,2) AS 'AñoAnterior', 
+	--Round(B.SaldoPrev,2) SaldoPrev, Round(B.Debe,2) Debe, Round(B.Haber,2) Haber, 
+	--Round(B.SaldoFinal,2) AS 'AñoActual'
+	--, T2.TypeAccountId, T3.TypeAccountName AS 'TipoDeCuenta'
+	--, T4.ParentAccountId AS 'SubCuentaId'
+	--, T5.Description AS 'SubCuenta'
+	--, B.HierarchyAccount AS 'Nivel'
+	--, ROW_NUMBER() OVER (ORDER BY B.AccountCode) AS 'Columna'
+	--FROM #BalanceSaldo B	
+	--LEFT JOIN Accounting T2 ON B.AccountId = T2.AccountId
+	--LEFT JOIN TypeAccount T3 ON T2.TypeAccountId = T3.TypeAccountId
+	--LEFT JOIN Accounting T4 ON T4.AccountId = T2.ParentAccountId
+	--LEFT JOIN Accounting T5 ON T4.ParentAccountId = T5.AccountId
+	--WHERE B.HierarchyAccount <= @NIVEL
+	----AND NOT (Round(B.SaldoFinal,2) = 0 AND Round(B.SaldoPrevAnio,2) = 0 AND B.HierarchyAccount > 2)
+	--AND NOT (Round(B.SaldoFinal,2) = 0 AND Round(B.SaldoPrevAnio,2) = 0)
+	--AND NOT (B.HierarchyAccount = 1 AND T2.TypeAccountId = 4)
+	--AND NOT (B.HierarchyAccount > 2 AND T2.TypeAccountId = 4)
+	--ORDER BY B.AccountCode
+
+	--DELETE FROM #NotasEstadosFinancieros WHERE Nivel < 2 AND SubCuentaId IS NULL AND SubCuenta IS NULL
+
 	INSERT INTO #NotasEstadosFinancieros
 	SELECT 
 	Nota = CASE WHEN B.HierarchyAccount != 2 THEN NULL ELSE ROW_NUMBER() OVER (PARTITION BY CASE WHEN B.HierarchyAccount != 2 THEN 1 ELSE 0 END ORDER BY B.AccountId) END + 3,
@@ -459,33 +488,194 @@ BEGIN
 	WHEN B.DeudoraAcreedora = 'A' THEN '2'
 	ELSE '1'
 	END AS 'DeudoraAcreedora', B.Estado, B.Totaliza, 
+	--CASE T2.AceptaNegativo
+	--WHEN 1 THEN
+	--	Round(B.SaldoPrevAnio,2) * -1
+	--ELSE
+	--	Round(B.SaldoPrevAnio,2)
+	--END
 	Round(B.SaldoPrevAnio,2) AS 'AñoAnterior', 
 	Round(B.SaldoPrev,2) SaldoPrev, Round(B.Debe,2) Debe, Round(B.Haber,2) Haber, 
+	--CASE T2.AceptaNegativo
+	--WHEN 1 THEN
+	--	Round(B.SaldoFinal,2) * -1
+	--ELSE
+	--	Round(B.SaldoFinal,2)
+	--END
 	Round(B.SaldoFinal,2) AS 'AñoActual'
 	, T2.TypeAccountId, T3.TypeAccountName AS 'TipoDeCuenta'
 	, T4.ParentAccountId AS 'SubCuentaId'
 	, T5.Description AS 'SubCuenta'
 	, B.HierarchyAccount AS 'Nivel'
-	, ROW_NUMBER() OVER (ORDER BY B.AccountCode) AS 'Columna'
+	, ROW_NUMBER() OVER (PARTITION BY B.DeudoraAcreedora ORDER BY B.AccountCode) AS 'Columna'
 	FROM #BalanceSaldo B	
 	LEFT JOIN Accounting T2 ON B.AccountId = T2.AccountId
 	LEFT JOIN TypeAccount T3 ON T2.TypeAccountId = T3.TypeAccountId
 	LEFT JOIN Accounting T4 ON T4.AccountId = T2.ParentAccountId
 	LEFT JOIN Accounting T5 ON T4.ParentAccountId = T5.AccountId
 	WHERE B.HierarchyAccount <= @NIVEL
-	--AND NOT (Round(B.SaldoFinal,2) = 0 AND Round(B.SaldoPrevAnio,2) = 0 AND B.HierarchyAccount > 2)
-	AND NOT (Round(B.SaldoFinal,2) = 0 AND Round(B.SaldoPrevAnio,2) = 0)
+	AND NOT (Round(B.SaldoFinal,2) = 0 AND Round(B.SaldoPrevAnio,2) = 0 AND B.HierarchyAccount > 2)
 	AND NOT (B.HierarchyAccount = 1 AND T2.TypeAccountId = 4)
 	AND NOT (B.HierarchyAccount > 2 AND T2.TypeAccountId = 4)
 	ORDER BY B.AccountCode
 
-	DELETE FROM #NotasEstadosFinancieros WHERE Nivel < 2 AND SubCuentaId IS NULL AND SubCuenta IS NULL
+	INSERT INTO #NotasEstadosFinancieros 
+	SELECT Nota, AccountId, AccountCode, 'TOTAL ACTIVO', ParentAccountId, DeudoraAcreedora, Estado, Totaliza, AñoAnterior, SaldoPrev, Debe, Haber, AñoActual
+	, TypeAccountId, 'TOTAL', SubCuentaId, SubCuenta, Nivel, (SELECT MAX(Columna) + 1 FROM #NotasEstadosFinancieros WHERE TypeAccountId = 1)
+	FROM #NotasEstadosFinancieros WHERE AccountCode = '1'
+
+	UPDATE #NotasEstadosFinancieros
+	SET AñoActual = NULL, AñoAnterior = NULL
+	WHERE AccountCode = '1' AND Descripcion != 'TOTAL ACTIVO'
+
+	UPDATE #NotasEstadosFinancieros
+	SET Columna = Columna + 1
+	WHERE TypeAccountId = 4 AND DeudoraAcreedora = '1'
+
+	INSERT INTO #NotasEstadosFinancieros 
+	SELECT Nota, AccountId, AccountCode, 'TOTAL PASIVO', ParentAccountId, DeudoraAcreedora, Estado, Totaliza, AñoAnterior, SaldoPrev, Debe, Haber, AñoActual
+	, TypeAccountId, 'TOTAL', SubCuentaId, SubCuenta, Nivel, (SELECT MAX(Columna) + 1 FROM #NotasEstadosFinancieros WHERE TypeAccountId = 2)
+	FROM #NotasEstadosFinancieros WHERE AccountCode = '2'
+
+	UPDATE #NotasEstadosFinancieros
+	SET AñoActual = NULL, AñoAnterior = NULL
+	WHERE AccountCode = '2' AND Descripcion != 'TOTAL PASIVO'
+
+	UPDATE #NotasEstadosFinancieros
+	SET Columna = Columna + 1
+	WHERE TypeAccountId = 3 AND DeudoraAcreedora = '2'
+
+	UPDATE #NotasEstadosFinancieros
+	SET Columna = Columna + 1
+	WHERE TypeAccountId = 4 AND DeudoraAcreedora = '2'
+
+	INSERT INTO #NotasEstadosFinancieros 
+	SELECT Nota, AccountId, AccountCode, 'TOTAL PATRIMONIO', ParentAccountId, DeudoraAcreedora, Estado, Totaliza, AñoAnterior, SaldoPrev, Debe, Haber, AñoActual
+	, TypeAccountId, 'TOTAL', SubCuentaId, SubCuenta, Nivel, (SELECT MAX(Columna) + 1 FROM #NotasEstadosFinancieros WHERE TypeAccountId = 3)
+	FROM #NotasEstadosFinancieros WHERE AccountCode = '3'
+
+	UPDATE #NotasEstadosFinancieros
+	SET AñoActual = NULL, AñoAnterior = NULL
+	WHERE AccountCode = '3' AND Descripcion != 'TOTAL PATRIMONIO'
+
+	UPDATE #NotasEstadosFinancieros
+	SET Columna = Columna + 1
+	WHERE TypeAccountId = 4 AND DeudoraAcreedora = '2'
+
+	INSERT INTO #NotasEstadosFinancieros 
+	SELECT TOP 1 NULL, NULL, NULL, 'TOTAL PASIVO + PATRIMONIO', NULL, '2', 'Activo', 1,
+	COALESCE((SELECT AñoAnterior FROM #NotasEstadosFinancieros WHERE Descripcion = 'TOTAL PASIVO'), 0) + COALESCE((SELECT AñoAnterior FROM #NotasEstadosFinancieros WHERE Descripcion = 'TOTAL PATRIMONIO'), 0)
+	AS AñoAnterior,
+	COALESCE((SELECT SaldoPrev FROM #NotasEstadosFinancieros WHERE Descripcion = 'TOTAL PASIVO'), 0) + COALESCE((SELECT SaldoPrev FROM #NotasEstadosFinancieros WHERE Descripcion = 'TOTAL PATRIMONIO'), 0)
+	AS SaldoPrev,
+	0,
+	0,
+	COALESCE((SELECT AñoActual FROM #NotasEstadosFinancieros WHERE Descripcion = 'TOTAL PASIVO'), 0) + COALESCE((SELECT AñoActual FROM #NotasEstadosFinancieros WHERE Descripcion = 'TOTAL PATRIMONIO'), 0)
+	AS AñoActual
+	, 3, 'TOTAL', NULL, NULL, 1, (SELECT MIN(Columna) FROM #NotasEstadosFinancieros WHERE TypeAccountId = 4 AND DeudoraAcreedora = '2')
+	FROM #NotasEstadosFinancieros 
+
+	UPDATE #NotasEstadosFinancieros
+	SET Columna = Columna + 1
+	WHERE TypeAccountId = 4 AND DeudoraAcreedora = '2'
+
+	INSERT INTO #NotasEstadosFinancieros 
+	SELECT TOP 1 NULL, NULL, NULL, 'TOTAL ACTIVO + CONTINGENTES', NULL, '1', 'Activo', 1,
+	COALESCE((SELECT AñoAnterior FROM #NotasEstadosFinancieros WHERE Descripcion = 'TOTAL ACTIVO'), 0) + COALESCE((SELECT SUM(AñoAnterior) FROM #NotasEstadosFinancieros WHERE TypeAccountId = 4 AND DeudoraAcreedora = '1' AND Nivel = 2), 0)
+	AS AñoAnterior,
+	COALESCE((SELECT SaldoPrev FROM #NotasEstadosFinancieros WHERE Descripcion = 'TOTAL ACTIVO'), 0) + COALESCE((SELECT SUM(SaldoPrev) FROM #NotasEstadosFinancieros WHERE TypeAccountId = 4 AND DeudoraAcreedora = '1' AND Nivel = 2), 0)
+	AS SaldoPrev,
+	0,
+	0,
+	COALESCE((SELECT AñoActual FROM #NotasEstadosFinancieros WHERE Descripcion = 'TOTAL ACTIVO'), 0) + COALESCE((SELECT SUM(AñoActual) FROM #NotasEstadosFinancieros WHERE TypeAccountId = 4 AND DeudoraAcreedora = '1' AND Nivel = 2), 0)
+	AS AñoActual
+	, 4, 'TOTAL', NULL, NULL, 1, (SELECT MAX(Columna) + 1 FROM #NotasEstadosFinancieros WHERE TypeAccountId = 4 AND DeudoraAcreedora = '1')
+	FROM #NotasEstadosFinancieros
+
+	INSERT INTO #NotasEstadosFinancieros 
+	SELECT TOP 1 NULL, NULL, NULL, 'TOTAL PASIVO + PATRIMONIO + CONTINGENTES', NULL, '2', 'Activo', 1,
+	COALESCE((SELECT AñoAnterior FROM #NotasEstadosFinancieros WHERE Descripcion = 'TOTAL PASIVO + PATRIMONIO'), 0) + COALESCE((SELECT SUM(AñoAnterior) FROM #NotasEstadosFinancieros WHERE TypeAccountId = 4 AND DeudoraAcreedora = '2' AND Nivel = 2), 0)
+	AS AñoAnterior,
+	COALESCE((SELECT SaldoPrev FROM #NotasEstadosFinancieros WHERE Descripcion = 'TOTAL PASIVO + PATRIMONIO'), 0) + COALESCE((SELECT SUM(SaldoPrev) FROM #NotasEstadosFinancieros WHERE TypeAccountId = 4 AND DeudoraAcreedora = '2' AND Nivel = 2), 0)
+	AS SaldoPrev,
+	0,
+	0,
+	COALESCE((SELECT AñoActual FROM #NotasEstadosFinancieros WHERE Descripcion = 'TOTAL PASIVO + PATRIMONIO'), 0) + COALESCE((SELECT SUM(AñoActual) FROM #NotasEstadosFinancieros WHERE TypeAccountId = 4 AND DeudoraAcreedora = '2' AND Nivel = 2), 0)
+	AS AñoActual
+	, 4, 'TOTAL', NULL, NULL, 1, (SELECT MAX(Columna) + 1 FROM #NotasEstadosFinancieros WHERE TypeAccountId = 4 AND DeudoraAcreedora = '2')
+	FROM #NotasEstadosFinancieros
+
+	DECLARE @CTotalActivo INT;
+	DECLARE @CTotalPasivoPatrimonio INT;
+	DECLARE @CTotalActivoContingentes INT;
+	DECLARE @CTotalPasivoPatrimonioContingentes INT;
+
+	SET @CTotalActivo = (SELECT Columna FROM #NotasEstadosFinancieros WHERE Descripcion = 'TOTAL ACTIVO')
+	SET @CTotalPasivoPatrimonio = (SELECT Columna FROM #NotasEstadosFinancieros WHERE Descripcion = 'TOTAL PASIVO + PATRIMONIO')
+
+	IF(@CTotalActivo != @CTotalPasivoPatrimonio)
+	BEGIN
+		IF(@CTotalActivo > @CTotalPasivoPatrimonio)
+		BEGIN
+			WHILE @CTotalActivo != @CTotalPasivoPatrimonio
+			BEGIN
+			INSERT INTO #NotasEstadosFinancieros VALUES (NULL, NULL, NULL, NULL, NULL, '2', 'Activo', 1, NULL, NULL, 0, 0, NULL, 3, 'BLANCO', NULL, NULL, 1, @CTotalPasivoPatrimonio)
+			SET @CTotalPasivoPatrimonio = @CTotalPasivoPatrimonio + 1
+			UPDATE #NotasEstadosFinancieros
+			SET Columna = @CTotalPasivoPatrimonio
+			WHERE Descripcion = 'TOTAL PASIVO + PATRIMONIO'
+			UPDATE #NotasEstadosFinancieros
+			SET Columna = Columna + 1
+			WHERE TypeAccountId = 4 AND DeudoraAcreedora = '2'
+			END
+		END
+		ELSE IF(@CTotalActivo < @CTotalPasivoPatrimonio)
+		BEGIN
+			WHILE @CTotalActivo != @CTotalPasivoPatrimonio
+			BEGIN
+			INSERT INTO #NotasEstadosFinancieros VALUES (NULL, NULL, NULL, NULL, NULL, '1', 'Activo', 1, NULL, NULL, 0, 0, NULL, 3, 'BLANCO', NULL, NULL, 1, @CTotalActivo)
+			SET @CTotalActivo = @CTotalActivo + 1
+			UPDATE #NotasEstadosFinancieros
+			SET Columna = @CTotalActivo
+			WHERE Descripcion = 'TOTAL ACTIVO'
+			UPDATE #NotasEstadosFinancieros
+			SET Columna = Columna + 1
+			WHERE TypeAccountId = 4 AND DeudoraAcreedora = '1'
+			END
+		END
+	END
+
+	SET @CTotalActivoContingentes = (SELECT Columna FROM #NotasEstadosFinancieros WHERE Descripcion = 'TOTAL ACTIVO + CONTINGENTES')
+	SET @CTotalPasivoPatrimonioContingentes = (SELECT Columna FROM #NotasEstadosFinancieros WHERE Descripcion = 'TOTAL PASIVO + PATRIMONIO + CONTINGENTES')
+
+	IF(@CTotalActivoContingentes != @CTotalPasivoPatrimonioContingentes)
+	BEGIN
+		IF(@CTotalActivoContingentes > @CTotalPasivoPatrimonioContingentes)
+		BEGIN
+			WHILE @CTotalActivoContingentes != @CTotalPasivoPatrimonioContingentes
+			BEGIN
+			INSERT INTO #NotasEstadosFinancieros VALUES (NULL, NULL, NULL, NULL, NULL, '2', 'Activo', 1, NULL, NULL, 0, 0, NULL, 3, 'BLANCO', NULL, NULL, 1, @CTotalPasivoPatrimonioContingentes)
+			SET @CTotalPasivoPatrimonioContingentes = @CTotalPasivoPatrimonioContingentes + 1
+			UPDATE #NotasEstadosFinancieros
+			SET Columna = @CTotalPasivoPatrimonioContingentes
+			WHERE Descripcion = 'TOTAL ACTIVO + CONTINGENTES'
+			END
+		END
+		ELSE IF(@CTotalActivoContingentes < @CTotalPasivoPatrimonioContingentes)
+		BEGIN
+			WHILE @CTotalActivoContingentes != @CTotalPasivoPatrimonioContingentes
+			BEGIN
+			INSERT INTO #NotasEstadosFinancieros VALUES (NULL, NULL, NULL, NULL, NULL, '1', 'Activo', 1, NULL, NULL, 0, 0, NULL, 3, 'BLANCO', NULL, NULL, 1, @CTotalActivoContingentes)
+			SET @CTotalActivoContingentes = @CTotalActivoContingentes + 1
+			UPDATE #NotasEstadosFinancieros
+			SET Columna = @CTotalActivoContingentes
+			WHERE Descripcion = 'TOTAL PASIVO + PATRIMONIO + CONTINGENTES'
+			END
+		END
+	END
 
 	SELECT Nota, Descripcion FROM #NotasEstadosFinancieros T0
 	WHERE Nota IS NOT NULL
-	ORDER BY T0.TypeAccountId, T0.Columna
+	ORDER BY T0.Nota
 	
-END		
-GO
-
-
+END
