9a51ecfbea40a81475fe3fad559592b4a011a944
AnthonyFugon <44510991+AnthonyFugon@users.noreply.github.com>
2020-03-23 14:39:46 -0600

    ContextoRiesgo CRUD

---
+++ ERPAPI/Models/ContextoRiesgo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERPAPI.Models
+{
+    public class ContextoRiesgo
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Int64 IdContextoRiesgo { get; set; }
+        public string TipoContexto { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaModificacion { get; set; }
+        public string UsuarioCreacion { get; set; }
+        public string UsuarioModificacion { get; set; }
+    }
+}
