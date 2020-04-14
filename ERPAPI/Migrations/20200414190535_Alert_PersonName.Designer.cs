c717d8e126c94e9f780fcd11c422a26b86146fe1
Alfredo Matute Ochoa <pidgy@hotmail.es>
2020-04-14 13:18:09 -0600

    Agregar campo PersonName a la base de datos

--- ERPAPI/Migrations/20200414190535_Alert_PersonName.Designer.cs
+++ ERPAPI/Migrations/20200414190535_Alert_PersonName.Designer.cs
@@ -4,14 +4,16 @@ using ERP.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ERPAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200414190535_Alert_PersonName")]
+    partial class Alert_PersonName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
