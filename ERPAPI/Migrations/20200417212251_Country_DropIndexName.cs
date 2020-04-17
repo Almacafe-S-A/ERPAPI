87cd10a5476df5f9063ddfc5614202bc13c3f9ba
Alfredo Matute Ochoa <pidgy@hotmail.es>
2020-04-17 15:35:44 -0600

    Quitar indice de nombres duplicados para pais en la base de datos

---
+++ ERPAPI/Migrations/20200417212251_Country_DropIndexName.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERPAPI.Migrations
+{
+    public partial class Country_DropIndexName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Country_Name",
+                table: "Country");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Country",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Country",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Country_Name",
+                table: "Country",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+        }
+    }
+}
