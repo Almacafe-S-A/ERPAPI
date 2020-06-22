806efd1f0fb2ee5ccfb40854658a6e0c71a7fe36
Carlos Eduardo Castillo <carlos.castillo@bi-dss..com>
2020-06-22 16:16:58 -0600

    Quitar restricciones de requeridos de los modelos  en tasas de cambio

--- ERPAPI/Models/ExchangeRate.cs
+++ ERPAPI/Models/ExchangeRate.cs
@@ -35,19 +35,19 @@ namespace ERPAPI.Models
        
         public string CurrencyName { get; set; }
 
-        [Required]
+        
         [Display(Name = "Usuario que lo crea")]
         public string CreatedUser { get; set; }
 
-        [Required]
+       
         [Display(Name = "Usuario que lo modifica")]
         public string ModifiedUser { get; set; }
 
-        [Required]
+        
         [Display(Name = "Fecha de creacion")]
         public DateTime CreatedDate { get; set; }
 
-        [Required]
+       
         [Display(Name = "Fecha de Modificacion")]
         public DateTime ModifiedDate { get; set; }
     }
