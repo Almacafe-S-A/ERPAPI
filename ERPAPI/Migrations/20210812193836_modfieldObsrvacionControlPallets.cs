6aaf53464fd22d6f32fbe57cc9fe1a918fb4f653
Carlos Eduardo Castillo <carlosedcastillo@outlook.com>
2021-08-12 14:45:33 -0600

    Cambios a Control de estibas

---
+++ ERPAPI/Migrations/20210812193836_modfieldObsrvacionControlPallets.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERPAPI.Migrations
+{
+    public partial class modfieldObsrvacionControlPallets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Observacion",
+                table: "ControlPallets",
+                newName: "Observaciones");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Observaciones",
+                table: "ControlPallets",
+                newName: "Observacion");
+        }
+    }
+}
