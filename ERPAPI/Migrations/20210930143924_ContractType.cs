96e4aff652c147409c9f19f3eadaba181fd12f68
Carlos Eduardo Castillo <carlosedcastillo@outlook.com>
2021-09-30 09:12:40 -0600

    Agregada la funcionalidad de Adendum

---
+++ ERPAPI/Migrations/20210930143924_ContractType.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERPAPI.Migrations
+{
+    public partial class ContractType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TypeContractName",
+                table: "CustomerContract",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TypeContractName",
+                table: "CustomerContract");
+        }
+    }
+}
