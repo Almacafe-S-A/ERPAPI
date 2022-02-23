4cb86f02ca8ad219baec5fd6bfe02d4d5e6cbe22
Carlos Eduardo Castillo <carlosedcastillo@outlook.com>
2022-02-23 06:27:16 -0600

    Inventario Fisico Modelo Coreeccion

---
+++ ERPAPI/Migrations/20220223122104_CambioNombreCampoInventarioFisico.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERPAPI.Migrations
+{
+    public partial class CambioNombreCampoInventarioFisico : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "InventarioFisico",
+                table: "InventarioFisicoLines",
+                newName: "InventarioFisicoCantidad");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "InventarioFisicoCantidad",
+                table: "InventarioFisicoLines",
+                newName: "InventarioFisico");
+        }
+    }
+}
