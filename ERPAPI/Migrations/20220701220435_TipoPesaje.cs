dcda90680b84b8ab3fc39fa6fd11e74c589ac88a
Carlos Eduardo Castillo <carlosedcastillo@outlook.com>
2022-07-01 17:18:36 -0600

    Agregada clasificacion de las boletas de peso

---
+++ ERPAPI/Migrations/20220701220435_TipoPesaje.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERPAPI.Migrations
+{
+    public partial class TipoPesaje : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Ingreso",
+                table: "Boleto_Ent",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ingreso",
+                table: "Boleto_Ent");
+        }
+    }
+}
