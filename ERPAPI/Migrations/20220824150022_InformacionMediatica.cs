94fd8f84299ec93cf326b4c63659e3cdc5e753f0
Carlos Eduardo Castillo <carlosedcastillo@outlook.com>
2022-08-24 09:12:53 -0600

    Informacion Mediatica

---
+++ ERPAPI/Migrations/20220824150022_InformacionMediatica.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERPAPI.Migrations
+{
+    public partial class InformacionMediatica : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "CustomerId",
+                table: "BlackListCustomers",
+                nullable: true,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nombre",
+                table: "BlackListCustomers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Nombre",
+                table: "BlackListCustomers");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "CustomerId",
+                table: "BlackListCustomers",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldNullable: true);
+        }
+    }
+}
