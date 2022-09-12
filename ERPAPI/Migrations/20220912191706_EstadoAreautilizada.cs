2c6612728987632eeb5ab2a61c6a284a00952b8b
GTT <carlosedcastillo@outlook.com>
2022-09-12 13:45:21 -0600

    Areas Ocupadas en bodega

---
+++ ERPAPI/Migrations/20220912191706_EstadoAreautilizada.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERPAPI.Migrations
+{
+    public partial class EstadoAreautilizada : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Cerrado",
+                table: "CustomerArea",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cerrado",
+                table: "CustomerArea");
+        }
+    }
+}
