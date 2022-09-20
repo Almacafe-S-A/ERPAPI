a3c01d28e7ae40a801506847e620f814184d039d
GTT <carlosedcastillo@outlook.com>
2022-09-20 12:08:29 -0600

    Cambios en Activos Fijos

--- ERPAPI/Migrations/20220920174620_AddedFieldAssets.Designer.cs
+++ ERPAPI/Migrations/20220920174620_AddedFieldAssets.Designer.cs
@@ -4,14 +4,16 @@ using ERP.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ERPAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220920174620_AddedFieldAssets")]
+    partial class AddedFieldAssets
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
