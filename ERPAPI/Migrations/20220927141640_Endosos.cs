832d7f75a650aa3ba51782e905245369c31fa4e2
Carlos Castillo <carlosedcastillo@outlook.com>
2022-09-27 08:16:56 -0600

    Endosos

---
+++ ERPAPI/Migrations/20220927141640_Endosos.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERPAPI.Migrations
+{
+    public partial class Endosos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "DerechosFiscales",
+                table: "EndososCertificadosLine",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Pda",
+                table: "EndososCertificadosLine",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ValorUnitarioDerechos",
+                table: "EndososCertificadosLine",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DerechosFiscales",
+                table: "EndososCertificadosLine");
+
+            migrationBuilder.DropColumn(
+                name: "Pda",
+                table: "EndososCertificadosLine");
+
+            migrationBuilder.DropColumn(
+                name: "ValorUnitarioDerechos",
+                table: "EndososCertificadosLine");
+        }
+    }
+}
