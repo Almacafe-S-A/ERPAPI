dcadaf0d83d4def5b4c34e25e05e430210fb0c22
Carlos Castillo <carlosedcastillo@outlook.com>
2022-11-08 15:54:02 -0600

    Agregado Campo SituadoEn en la Solicitud de Certificado

---
+++ ERPAPI/Migrations/20221108214855_AddSituadoEnSCD.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERPAPI.Migrations
+{
+    public partial class AddSituadoEnSCD : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SituadoEn",
+                table: "SolicitudCertificadoDeposito",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SituadoEn",
+                table: "SolicitudCertificadoDeposito");
+        }
+    }
+}
