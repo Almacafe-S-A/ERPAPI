f19f52950301741ffbc6eac1b9680d0012c13077
Carlos Eduardo Castillo <carlosedcastillo@outlook.com>
2023-03-22 16:07:19 -0600

    Repoortes de Monitoreo

---
+++ ERPAPI/Migrations/20230322214638_CustomerFields.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERPAPI.Migrations
+{
+    public partial class CustomerFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AFND",
+                table: "Customer",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaBaja",
+                table: "Customer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "PEP",
+                table: "Customer",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AFND",
+                table: "Customer");
+
+            migrationBuilder.DropColumn(
+                name: "FechaBaja",
+                table: "Customer");
+
+            migrationBuilder.DropColumn(
+                name: "PEP",
+                table: "Customer");
+        }
+    }
+}
