47265527e1e81bd647b5c77d2c5e8d9521f7b378
Carlos Eduardo Castillo <carlosedcastillo@outlook.com>
2023-05-26 12:40:07 -0600

    Comprobante de Retencion

---
+++ ERPAPI/Migrations/20230526170806_ComprobanteRetencionDocumentoComprobante.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERPAPI.Migrations
+{
+    public partial class ComprobanteRetencionDocumentoComprobante : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CAIDocumento",
+                table: "RetentionReceipt",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaLimiteDocumento",
+                table: "RetentionReceipt",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CAIDocumento",
+                table: "RetentionReceipt");
+
+            migrationBuilder.DropColumn(
+                name: "FechaLimiteDocumento",
+                table: "RetentionReceipt");
+        }
+    }
+}
