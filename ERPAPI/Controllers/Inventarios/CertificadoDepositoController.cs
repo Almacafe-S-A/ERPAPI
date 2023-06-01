cc25ea08a1901db3bed5834445a7eb85cb0a26d8
Rafael Suazo <supahotfire16@hotmail.com>
2023-06-01 11:36:19 -0600

    292INC Errores en Endosos Cambio#3

--- ERPAPI/Controllers/Inventarios/CertificadoDepositoController.cs
+++ ERPAPI/Controllers/Inventarios/CertificadoDepositoController.cs
@@ -283,12 +283,11 @@ namespace ERPAPI.Controllers
             List<EndososCertificados> endosos = new List<EndososCertificados>();
             try
             {
-                Items = await _context.CertificadoDeposito.Where(q => q.CustomerId == CustomerId).ToListAsync();
+                Items = await _context.CertificadoDeposito.Where(q => q.CustomerId == CustomerId && q.IdEstado == 6).ToListAsync();
 
                 endosos = await _context.EndososCertificados.Where(q => q.CustomerId == CustomerId).ToListAsync();
 
                 Items = Items.Where(q => !endosos.Any(a => a.NoCD == q.IdCD)).ToList();
-
 
             }
             catch (Exception ex)
