8d463c7512d6b23de3ac97fbf45fe6f8efe0d89d
Rafael Suazo <supahotfire16@hotmail.com>
2023-08-30 09:43:38 -0600

    16INC Cambio precision decimal InventarioFisicoLine

---
+++ ERPAPI/Migrations/20230830154129_CambioPrecisionDecimal.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ERPAPI.Migrations
+{
+	public partial class CambioPrecisionDecimal : Migration
+	{
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.AlterColumn<decimal>(
+				name: "SaldoLibros",
+				table: "InventarioFisicoLines",
+				type: "decimal(18, 6)", // Nueva precisión
+				nullable: false,
+				oldClrType: typeof(decimal),
+				oldType: "decimal(18, 2)"); // Precisión anterior
+
+			migrationBuilder.AlterColumn<decimal>(
+				name: "InventarioFisicoCantidad",
+				table: "InventarioFisicoLines",
+				type: "decimal(18, 6)", // Nueva precisión
+				nullable: false,
+				oldClrType: typeof(decimal),
+				oldType: "decimal(18, 2)"); // Precisión anterior
+
+			migrationBuilder.AlterColumn<decimal>(
+				name: "Diferencia",
+				table: "InventarioFisicoLines",
+				type: "decimal(18, 6)", // Nueva precisión
+				nullable: false,
+				oldClrType: typeof(decimal),
+				oldType: "decimal(18, 2)"); // Precisión anterior
+
+			migrationBuilder.AlterColumn<decimal>(
+				name: "NSacos",
+				table: "InventarioFisicoLines",
+				type: "decimal(18, 6)", // Nueva precisión
+				nullable: true,
+				oldClrType: typeof(decimal),
+				oldType: "decimal(18, 2)"); // Precisión anterior
+
+			migrationBuilder.AlterColumn<decimal>(
+				name: "FactorSacos",
+				table: "InventarioFisicoLines",
+				type: "decimal(18, 6)", // Nueva precisión
+				nullable: true,
+				oldClrType: typeof(decimal),
+				oldType: "decimal(18, 2)"); // Precisión anterior
+		}
+
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.AlterColumn<decimal>(
+				name: "SaldoLibros",
+				table: "InventarioFisicoLines",
+				type: "decimal(18, 2)", // Precisión original
+				nullable: false,
+				oldClrType: typeof(decimal),
+				oldType: "decimal(18, 6)"); // Nueva precisión
+
+			migrationBuilder.AlterColumn<decimal>(
+				name: "InventarioFisicoCantidad",
+				table: "InventarioFisicoLines",
+				type: "decimal(18, 2)", // Precisión original
+				nullable: false,
+				oldClrType: typeof(decimal),
+				oldType: "decimal(18, 6)"); // Nueva precisión
+
+			migrationBuilder.AlterColumn<decimal>(
+				name: "Diferencia",
+				table: "InventarioFisicoLines",
+				type: "decimal(18, 2)", // Precisión original
+				nullable: false,
+				oldClrType: typeof(decimal),
+				oldType: "decimal(18, 6)"); // Nueva precisión
+
+			migrationBuilder.AlterColumn<decimal>(
+				name: "NSacos",
+				table: "InventarioFisicoLines",
+				type: "decimal(18, 2)", // Precisión original
+				nullable: true,
+				oldClrType: typeof(decimal),
+				oldType: "decimal(18, 6)"); // Nueva precisión
+
+			migrationBuilder.AlterColumn<decimal>(
+				name: "FactorSacos",
+				table: "InventarioFisicoLines",
+				type: "decimal(18, 2)", // Precisión original
+				nullable: true,
+				oldClrType: typeof(decimal),
+				oldType: "decimal(18, 6)"); // Nueva precisión
+		}
+	}
+}
