28f7a6f8172ef9017df667b00085c1c81b167457
Rafael Suazo <supahotfire16@hotmail.com>
2023-10-19 15:17:05 -0600

    339INC Cambio Controller y modelo Feriado

--- ERPAPI/Controllers/RRHH/FeriadoController.cs
+++ ERPAPI/Controllers/RRHH/FeriadoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ERP.Contexts;
 using ERPAPI.Contexts;
+using ERPAPI.Migrations;
 using ERPAPI.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
@@ -11,6 +12,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using static Microsoft.AspNetCore.Hosting.Internal.HostingApplication;
 
 namespace ERPAPI.Controllers
@@ -29,84 +31,6 @@ namespace ERPAPI.Controllers
             this.logger = logger;
         }
 
-        [HttpPost("[action]")]
-        public async Task<ActionResult> Guardar(Feriado feriado)
-        {
-            try
-            {
-                if (feriado.Id == 0)
-                {
-                    Feriado f =  context.Feriados
-                        .Where(q => q.Nombre == feriado.Nombre && q.PeriodoId == feriado.PeriodoId 
-                        && ((q.FechaInicio >= feriado.FechaInicio &&  feriado.FechaInicio<=q.FechaFin) ||
-                        (q.FechaFin >= feriado.FechaFin && feriado.FechaFin<=q.FechaFin ))).FirstOrDefault();
-                    if (f != null)
-                    {
-                        return BadRequest("Ya existe un Feriado con esta configuracion");
-                    }
-                    context.Feriados.Add(feriado);
-
-                    //YOJOCASU 2022-02-26 REGISTRO DE LOS DATOS DE AUDITORIA
-                    new appAuditor(context, logger, User.Identity.Name).SetAuditor();
-
-                    await context.SaveChangesAsync();
-                    return Ok(feriado);
-                }
-                else
-                {
-                    Feriado feriadoExistente = await context.Feriados.FirstOrDefaultAsync(f => f.Id == feriado.Id);
-                    if (feriadoExistente == null)
-                    {
-                        throw new Exception("Registro de feriado a actualizar no existe");
-                    }
-
-                    context.Entry(feriadoExistente).CurrentValues.SetValues(feriado);
-
-                    //YOJOCASU 2022-02-26 REGISTRO DE LOS DATOS DE AUDITORIA
-                    new appAuditor(context, logger, User.Identity.Name).SetAuditor();
-
-                    await context.SaveChangesAsync();
-                    return Ok(feriadoExistente);
-                }
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, "Error al guardar el registro del feriado");
-                return BadRequest(ex);
-            }
-        }
-
-        [HttpPost("[action]")]
-        public async Task<ActionResult> Delete(Feriado feriado)
-        {
-            try
-            {
-                var vferiado = context.Feriados.Where(q => q.Id == feriado.Id).FirstOrDefault();
-                if (vferiado!=null)
-                {
-                    context.Feriados.Remove(vferiado);
-
-                    //YOJOCASU 2022-02-26 REGISTRO DE LOS DATOS DE AUDITORIA
-                    new appAuditor(context, logger, User.Identity.Name).SetAuditor();
-
-                    await context.SaveChangesAsync();
-                    return Ok(feriado);
-                }
-                else
-                {
-                    return BadRequest("No se encontro el Feriado");
-                }
-
-                
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, "Error al guardar el registro del feriado");
-                return BadRequest(ex);
-            }
-        }
-
-
         [HttpGet("[action]")]
         public async Task<ActionResult> GetFeriados()
         {
@@ -121,21 +45,6 @@ namespace ERPAPI.Controllers
                 return BadRequest(ex);
             }
         }
-
-    /*    [HttpGet("[action]/{anio}")]
-        public async Task<ActionResult> GetFeriadoAnio(int P)
-        {
-            try
-            {
-                var feriados = await context.Feriados.Include(e=>e.Estado).Where(f=>f.PeriodoId==anio).ToListAsync();
-                return Ok(feriados);
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, "Error al cargar feriados por año");
-                return BadRequest(ex);
-            }
-        }*/
 
         [HttpGet("[action]/{fecha}")]
         public async Task<ActionResult> GetFeriadoFecha(DateTime fecha)
@@ -176,42 +85,130 @@ namespace ERPAPI.Controllers
             return await Task.Run(() => Ok(Items));
         }
 
-        [HttpPut("[action]")]
-        public async Task<ActionResult<Feriado>> Update(Feriado feriado)
+        [HttpGet("[action]/{Id}")]
+        public async Task<IActionResult> GetFeriadoById(Int64 Id)
         {
+            Feriado Items = new Feriado();
             try
             {
-                Feriado feriadoExistente = await context.Feriados.FirstOrDefaultAsync(f => f.Id == feriado.Id);
-                if (feriadoExistente == null)
-                {
-                    return NotFound("Registro de feriado a actualizar no existe");
-                }
-
-                // Verificar si existe otro feriado con el mismo nombre en el mismo PeriodoId
-                Feriado feriadoDuplicado = await context.Feriados
-                    .FirstOrDefaultAsync(f => f.Nombre == feriado.Nombre && f.PeriodoId == feriado.PeriodoId && f.Id != feriado.Id);
-                if (feriadoDuplicado != null)
-                {
-                    return BadRequest("Ya existe un Feriado con este nombre en el mismo PeriodoId");
-                }
-
-                // Actualizar las propiedades del feriado existente con los valores del feriado recibido
-                context.Entry(feriadoExistente).CurrentValues.SetValues(feriado);
-
-                // Actualizar los datos de auditoría
-                new appAuditor(context, logger, User.Identity.Name).SetAuditor();
-
-                await context.SaveChangesAsync();
-                return Ok(feriadoExistente);
+                Items = await context.Feriados.Where(q => q.Id == Id).FirstOrDefaultAsync();
             }
             catch (Exception ex)
             {
-                logger.LogError(ex, "Error al actualizar el registro del feriado");
-                return BadRequest("Error al actualizar el registro del feriado");
+                logger.LogError($"Ocurrio un error: {ex.ToString()}");
+                return BadRequest($"Ocurrio un error:{ex.Message}");
             }
+            return await Task.Run(() => Ok(Items));
         }
 
+        [HttpGet("[action]/{Name}")]
+        public async Task<IActionResult> GetFeriadoByName(String Name)
+        {
+            Feriado Items = new Feriado();
+            try
+            {
+                Items = await context.Feriados.Where(q => q.Nombre == Name).FirstOrDefaultAsync();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Ocurrio un error: {ex.ToString()}");
+                return BadRequest($"Ocurrio un error:{ex.Message}");
+            }
+            return await Task.Run(() => Ok(Items));
+        }
+
+        /// <summary>
+        /// Inserta un nuevo Feriado
+        /// </summary>
+        /// <param name="_Feriado"></param>
+        /// <returns></returns>
+        [HttpPost("[action]")]
+        public async Task<ActionResult<Feriado>> Insert([FromBody] Feriado _Feriado)
+        {
+            Feriado _Feriadoq = new Feriado();
+            try
+            {
+                
+                _Feriadoq = _Feriado;
+                context.Feriados.Add(_Feriadoq);
+
+                //YOJOCASU 2022-02-26 REGISTRO DE LOS DATOS DE AUDITORIA
+                new appAuditor(context, logger, User.Identity.Name).SetAuditor();
+
+                await context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                logger.LogError($"Ocurrio un error: {ex.ToString()}");
+                return BadRequest($"Ocurrio un error:{ex.Message}");
+            }
+
+            return await Task.Run(() => Ok(_Feriadoq));
+        }
+
+        /// <summary>
+        /// Update un Feriado
+        /// </summary>
+        /// <param name="_Feriado"></param>
+        /// <returns></returns>
+        [HttpPut("[action]")]
+        public async Task<ActionResult<Feriado>> Update(Feriado _Feriado)
+        {
+            Feriado _Feriadoq = _Feriado;
+            try
+            {
+                _Feriadoq = await (from c in context.Feriados
+                                    .Where(q => q.Id == _Feriado.Id)
+                                   select c
+                                  ).FirstOrDefaultAsync();
+                context.Entry(_Feriadoq).CurrentValues.SetValues((_Feriado));
+
+                //YOJOCASU 2022-02-26 REGISTRO DE LOS DATOS DE AUDITORIA
+                new appAuditor(context, logger, User.Identity.Name).SetAuditor();
+
+                await context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                logger.LogError($"Ocurrio un error: {ex.ToString()}");
+                return BadRequest($"Ocurrio un error:{ex.Message}");
+            }
+            return await Task.Run(() => Ok(_Feriadoq));
+        }
+
+        /// <summary>
+        /// Elimina un feriado       
+        /// </summary>
+        /// <param name="_Feriado"></param>
+        /// <returns></returns>
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Delete([FromBody] Feriado _Feriado)
+        {
+            Feriado _Feriadoq = new Feriado();
+            try
+            {
+                _Feriadoq = context.Feriados
+                .Where(x => x.Id == (Int64)_Feriado.Id)
+                .FirstOrDefault();
+
+                context.Feriados.Remove(_Feriadoq);
+
+                //YOJOCASU 2022-02-26 REGISTRO DE LOS DATOS DE AUDITORIA
+                new appAuditor(context, logger, User.Identity.Name).SetAuditor();
 
 
+                await context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Ocurrio un error: {ex.ToString()}");
+                return BadRequest($"Ocurrio un error:{ex.Message}");
+            }
+
+            return await Task.Run(() => Ok(_Feriadoq));
+
+        }
     }
 }
